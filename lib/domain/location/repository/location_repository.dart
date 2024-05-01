9dbbdd20d5e34fd1ccbcff6c3c9ec2f57e33f4e0
SeongJongHo <whdgh9595@gmail.com>
2024-05-02 00:56:27 +0900

    Modify: LocationRepository 인터페이스 수정

--- lib/domain/location/repository/location_repository.dart
+++ lib/domain/location/repository/location_repository.dart
@@ -1,7 +1,3 @@
-import 'package:weaco/domain/location/model/location.dart';
-
 abstract interface class LocationRepository {
-  Future<Location> getLocation({int? lat, int? lng});
 
-  Future<void> saveLocation({Location location});
 }
