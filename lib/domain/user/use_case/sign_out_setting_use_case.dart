18782541333a29a910d7e9f348efd3a7cd920fdf
nala0897@naver.com <nala0897@naver.com>
2024-05-02 22:42:26 +0900

    Modify: 파일명 변경(logout->signOut)

--- lib/domain/user/use_case/sign_out_setting_use_case.dart
+++ lib/domain/user/use_case/sign_out_setting_use_case.dart
@@ -1,14 +1,14 @@
 import 'package:weaco/domain/user/repository/user_auth_repository.dart';
 
-class LogOutSettingUseCase {
+class SignOutSettingUseCase {
   final UserAuthRepository _userAuthRepository;
 
-  LogOutSettingUseCase({
+  SignOutSettingUseCase({
     required UserAuthRepository userAuthRepository,
   }) : _userAuthRepository = userAuthRepository;
 
   /// 설정 화면의 회월 탈퇴를 위한 Use Case
   Future<bool> execute({required String email}) async {
-    return await _userAuthRepository.logOutSetting(email: email);
+    return await _userAuthRepository.signOutSetting(email: email);
   }
 }
