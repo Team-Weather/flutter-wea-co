00c589a8feb89e5ac96851635f1d03135b2289d9
nala0897@naver.com <nala0897@naver.com>
2024-05-02 15:07:49 +0900

    Feat: GetDetailUserProfileUseCase 작성

--- lib/domain/user/repository/user_profile_repository.dart
+++ lib/domain/user/repository/user_profile_repository.dart
@@ -1,1 +1,5 @@
-abstract interface class UserProfileRepository {}
+import 'package:weaco/domain/user/model/user_profile.dart';
+
+abstract interface class UserProfileRepository {
+  Future<UserProfile?> getDetailUserProfile({required String email});
+}
