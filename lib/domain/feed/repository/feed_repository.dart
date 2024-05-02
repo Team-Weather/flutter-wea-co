f4937bcb37a318b73025382b62cf818fa160e9ad
hoogom88 <ziziwhdgus@naver.com>
2024-05-02 23:39:26 +0900

    Refactor: FeedRepository 인터페이스, MockFeedRepositoryImpl 충돌 해결

--- lib/domain/feed/repository/feed_repository.dart
+++ lib/domain/feed/repository/feed_repository.dart
@@ -2,4 +2,10 @@ import 'package:weaco/domain/feed/model/feed.dart';
 
 abstract interface class FeedRepository {
   Future<Feed?> getFeed({required String id});
+
+  Future<List<Feed>> getRecommendedFeeds(
+      {int? seasonCode,
+        int? weatherCode,
+        int? minTemperature,
+        int? maxTemperature});
 }
