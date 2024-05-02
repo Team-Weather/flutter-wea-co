a74bdc8d78037f44306e83161bee84d2d56486ff
nala0897@naver.com <nala0897@naver.com>
2024-05-02 16:51:46 +0900

    Modify: 주석 추가

--- lib/domain/user/use_case/log_out_setting_use_case.dart
+++ lib/domain/user/use_case/log_out_setting_use_case.dart
@@ -7,6 +7,7 @@ class LogOutSettingUseCase {
     required UserAuthRepository userAuthRepository,
   }) : _userAuthRepository = userAuthRepository;
 
+  /// 설정 화면의 회월 탈퇴를 위한 Use Case
   Future<bool> execute({required String email}) async {
     return await _userAuthRepository.logOutSetting(email: email);
   }
