74daa031487d1085fb290e06ae2a28d9857ecf22
SeongJongHo <whdgh9595@gmail.com>
2024-05-03 00:40:24 +0900

    Feat: UserProfileRepository에 getMyProfile 추상메소드 추가

--- lib/domain/user/repository/user_profile_repository.dart
+++ lib/domain/user/repository/user_profile_repository.dart
@@ -2,4 +2,6 @@ import 'package:weaco/domain/user/model/user_profile.dart';
 
 abstract interface class UserProfileRepository {
   Future<UserProfile?> getUserProfile({required String email});
+
+  Future<UserProfile?> getMyProfile();
 }
