872c5348625c70e573663c2f51bcdb3053dd0519
YunZi.Song <yoonji5809@gmail.com>
2024-05-02 12:00:45 +0900

    Feat: GetMyPageFeedsUseCase 작성

--- lib/domain/feed/repository/feed_repository.dart
+++ lib/domain/feed/repository/feed_repository.dart
@@ -1,1 +1,6 @@
-abstract interface class FeedRepository {}
+import 'package:weaco/domain/feed/model/feed.dart';
+
+abstract interface class FeedRepository {
+  /// 유저의 피드를 가져옵니다.
+  Future<List<Feed>> getFeeds({required String email, int? page});
+}
---
+++ lib/domain/feed/use_case/get_my_page_feeds_use_case.dart
@@ -0,0 +1,22 @@
+import 'package:weaco/domain/feed/model/feed.dart';
+import 'package:weaco/domain/feed/repository/feed_repository.dart';
+
+/// 마이 페이지에서 유저의 피드 목록을 보여주기 위한 Use Case
+class GetMyPageFeedsUseCase {
+  final FeedRepository feedRepository;
+
+  GetMyPageFeedsUseCase({required this.feedRepository});
+
+  Future<List<Feed>> execute({
+    required String email,
+    int? page = 1,
+  }) async {
+    List<Feed> feedList = [];
+    try {
+      feedList = await feedRepository.getFeeds(email: email, page: page);
+    } catch (e) {
+      throw Exception(e);
+    }
+    return feedList;
+  }
+}
