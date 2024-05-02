95306b5669ff977e0773ae0f8b6160e479defdcf
nala0897@naver.com <nala0897@naver.com>
2024-05-02 21:30:31 +0900

    Feat: GetDetailProfileUseCase 테스트 코드 수정, setUp 추가

--- test/data/user/repository/mock_user_profile_repository_impl.dart
+++ test/data/user/repository/mock_user_profile_repository_impl.dart
@@ -2,14 +2,18 @@ import 'package:weaco/domain/user/model/user_profile.dart';
 import 'package:weaco/domain/user/repository/user_profile_repository.dart';
 
 class MockUserProfileRepositoryImpl implements UserProfileRepository {
-  final Map<String, UserProfile> _fakeUserProfileMap = {};
+  Map<String, UserProfile> _fakeUserProfileMap = {};
 
   @override
   Future<UserProfile?> getDetailUserProfile({required String email}) async {
-    return Future.value(_fakeUserProfileMap[email]);
+    return _fakeUserProfileMap[email];
   }
 
   void addProfile({required UserProfile userProfile}) {
     _fakeUserProfileMap[userProfile.email] = userProfile;
+  }
+
+  void initMockData() {
+    _fakeUserProfileMap = {};
   }
 }
