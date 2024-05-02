c02383534cd79d3db347f662278569ccd3719531
YunZi.Song <yoonji5809@gmail.com>
2024-05-03 01:09:21 +0900

    Refactor: FeedRepository getFeedList 함수 파라미터 수정

--- lib/domain/feed/repository/feed_repository.dart
+++ lib/domain/feed/repository/feed_repository.dart
@@ -2,5 +2,9 @@ import 'package:weaco/domain/feed/model/feed.dart';
 
 abstract interface class FeedRepository {
   /// 유저의 피드를 가져옵니다.
-  Future<List<Feed>> getFeedList({required String email, int? page});
+  Future<List<Feed>> getFeedList({
+    required String email,
+    required DateTime? createdAt,
+    required int? limit,
+  });
 }
