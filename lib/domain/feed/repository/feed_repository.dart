f3911d960df3c02706f3e8877c89bf3b0d1f4baa
YunZi.Song <yoonji5809@gmail.com>
2024-05-02 12:15:06 +0900

    Refactor: getFeeds 함수명 변경

--- lib/domain/feed/repository/feed_repository.dart
+++ lib/domain/feed/repository/feed_repository.dart
@@ -2,5 +2,5 @@ import 'package:weaco/domain/feed/model/feed.dart';
 
 abstract interface class FeedRepository {
   /// 유저의 피드를 가져옵니다.
-  Future<List<Feed>> getFeeds({required String email, int? page});
+  Future<List<Feed>> getFeedList({required String email, int? page});
 }
