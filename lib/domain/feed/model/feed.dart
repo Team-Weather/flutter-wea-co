5f65fef80fa9b7fd652fda41ab078d377259cf97
YunZi.Song <yoonji5809@gmail.com>
2024-05-02 12:01:38 +0900

    Modify: Feed model class 수정

--- lib/domain/feed/model/feed.dart
+++ lib/domain/feed/model/feed.dart
@@ -1,11 +1,14 @@
+import 'package:weaco/domain/location/model/location.dart';
+import 'package:weaco/domain/weather/model/weather.dart';
+
 class Feed {
   final int id;
   final String imagePath;
   final String userEmail;
   final String description;
-  final String weather;
+  final Weather weather;
   final int seasonCode;
-  final String location;
+  final Location location;
   final DateTime createdAt;
   final DateTime? deletedAt;
 
@@ -21,35 +24,60 @@ class Feed {
     this.deletedAt,
   });
 
-  @override
-  String toString() {
-    return 'Feed{id: $id, imagePath: $imagePath, userEmail: $userEmail, description: $description, weather: $weather, seasonCode: $seasonCode, location: $location, createdAt: $createdAt, deletedAt: $deletedAt}';
+  Feed copyWith({
+    int? id,
+    String? imagePath,
+    String? userEmail,
+    String? description,
+    Weather? weather,
+    int? seasonCode,
+    Location? location,
+    DateTime? createdAt,
+    DateTime? deletedAt,
+  }) {
+    return Feed(
+      id: id ?? this.id,
+      imagePath: imagePath ?? this.imagePath,
+      userEmail: userEmail ?? this.userEmail,
+      description: description ?? this.description,
+      weather: weather ?? this.weather,
+      seasonCode: seasonCode ?? this.seasonCode,
+      location: location ?? this.location,
+      createdAt: createdAt ?? this.createdAt,
+      deletedAt: deletedAt ?? this.deletedAt,
+    );
   }
 
   @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is Feed &&
-          runtimeType == other.runtimeType &&
-          id == other.id &&
-          imagePath == other.imagePath &&
-          userEmail == other.userEmail &&
-          description == other.description &&
-          weather == other.weather &&
-          seasonCode == other.seasonCode &&
-          location == other.location &&
-          createdAt == other.createdAt &&
-          deletedAt == other.deletedAt;
+  String toString() {
+    return 'Feed(id: $id, imagePath: $imagePath, userEmail: $userEmail, description: $description, weather: $weather, seasonCode: $seasonCode, location: $location, createdAt: $createdAt, deletedAt: $deletedAt)';
+  }
 
   @override
-  int get hashCode =>
-      id.hashCode ^
-      imagePath.hashCode ^
-      userEmail.hashCode ^
-      description.hashCode ^
-      weather.hashCode ^
-      seasonCode.hashCode ^
-      location.hashCode ^
-      createdAt.hashCode ^
-      deletedAt.hashCode;
+  bool operator ==(covariant Feed other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id &&
+        other.imagePath == imagePath &&
+        other.userEmail == userEmail &&
+        other.description == description &&
+        other.weather == weather &&
+        other.seasonCode == seasonCode &&
+        other.location == location &&
+        other.createdAt == createdAt &&
+        other.deletedAt == deletedAt;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        imagePath.hashCode ^
+        userEmail.hashCode ^
+        description.hashCode ^
+        weather.hashCode ^
+        seasonCode.hashCode ^
+        location.hashCode ^
+        createdAt.hashCode ^
+        deletedAt.hashCode;
+  }
 }
