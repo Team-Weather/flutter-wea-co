2959669a66e8510566c821109dbfc7d7d6bf96aa
KimDonghyeok <qoophon@gmail.com>
2024-05-03 14:24:18 +0900

    Test: MockFeedRepositoryImpl 수정

--- test/mock/data/feed/repository/mock_feed_repository_impl.dart
+++ test/mock/data/feed/repository/mock_feed_repository_impl.dart
@@ -5,6 +5,7 @@ class MockFeedRepositoryImpl implements FeedRepository {
   int getFeedListcallCount = 0;
   int getFeedCallCount = 0;
   int getRecommendedFeedsCallCount = 0;
+  int getSearchFeedsCallCount = 0;
   String getFeedParamId = '';
 
   // 메서드 호출시 인자 확인을 위한 map
@@ -39,6 +40,7 @@ class MockFeedRepositoryImpl implements FeedRepository {
     getFeedListcallCount = 0;
     getFeedCallCount = 0;
     getRecommendedFeedsCallCount = 0;
+    getSearchFeedsCallCount = 0;
     methodParameterMap.clear();
     _fakeFeedList.clear();
     getFeedResult = null;
@@ -101,5 +103,43 @@ class MockFeedRepositoryImpl implements FeedRepository {
   /// [_fakeFeedList]의 모든 피드 삭제
   void resetFeedList() {
     _fakeFeedList.clear();
+  }
+
+  /// [_fakeFeedList]에서 조건에 맞는 피드 데이터를 찾아서 리스트로 반환
+  /// 호출시 [getSearchFeedsCallCount] + 1
+  @override
+  Future<List<Feed>> getSearchFeeds(
+      {int? seasonCode,
+      int? weatherCode,
+      int? minTemperature,
+      int? maxTemperature}) {
+    getSearchFeedsCallCount++;
+    methodParameterMap['seasonCode'] = seasonCode;
+    methodParameterMap['weatherCode'] = weatherCode;
+    methodParameterMap['minTemperature'] = minTemperature;
+    methodParameterMap['maxTemperature'] = maxTemperature;
+
+    List<Feed> result = _fakeFeedList;
+
+    if (seasonCode != null) {
+      result =
+          result.where((element) => element.seasonCode == seasonCode).toList();
+    }
+
+    if (weatherCode != null) {
+      result = result
+          .where((element) => element.weather.code == weatherCode)
+          .toList();
+    }
+
+    if (minTemperature != null && maxTemperature != null) {
+      result = result
+          .where((element) =>
+              element.weather.temperature >= minTemperature &&
+              element.weather.temperature <= maxTemperature)
+          .toList();
+    }
+
+    return Future.value(result);
   }
 }
