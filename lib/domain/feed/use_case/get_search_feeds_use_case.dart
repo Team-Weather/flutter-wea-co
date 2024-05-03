f6e75a9b104e4f4699810b0facc427f2579b3677
KimDonghyeok <qoophon@gmail.com>
2024-05-03 14:16:56 +0900

    Feat: GetSearchFeedsUseCase 작성

---
+++ lib/domain/feed/use_case/get_search_feeds_use_case.dart
@@ -0,0 +1,27 @@
+import 'package:weaco/domain/feed/model/feed.dart';
+import 'package:weaco/domain/feed/repository/feed_repository.dart';
+
+class GetSearchFeedsUseCase {
+  final FeedRepository _feedRepository;
+
+  GetSearchFeedsUseCase({required FeedRepository feedRepository})
+      : _feedRepository = feedRepository;
+
+  /// 레포지토리에 필터를 통해 추천 피드를 요청
+  /// @param seasonCode: 계절 코드
+  /// @param weatherCode: 날씨 코드
+  /// @param minTemperature: 최저 기온
+  /// @param maxTemperature: 최고 기온
+  /// @return: 반환 받은 검색 피드 리스트
+  Future<List<Feed>> execute(
+      {int? seasonCode,
+      int? weatherCode,
+      int? minTemperature,
+      int? maxTemperature}) async {
+    return await _feedRepository.getSearchFeeds(
+        seasonCode: seasonCode,
+        weatherCode: weatherCode,
+        minTemperature: minTemperature,
+        maxTemperature: maxTemperature);
+  }
+}
