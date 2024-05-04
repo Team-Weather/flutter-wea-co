71f85132714bf5e3a234ea28e138e626af5e7059
KimDonghyeok <qoophon@gmail.com>
2024-05-05 05:39:55 +0900

    Test: MockGetMyPageFeedsUseCase 작성

---
+++ test/mock/data/feed/use_case/mock_get_my_page_feeds_use_case.dart
@@ -0,0 +1,33 @@
+import 'package:weaco/domain/feed/model/feed.dart';
+import 'package:weaco/domain/feed/use_case/get_my_page_feeds_use_case.dart';
+
+class MockGetMyPageFeedsUseCase implements GetMyPageFeedsUseCase {
+  int executeCallCount = 0;
+  Map<String, dynamic> methodParameterMap = {};
+  List<Feed> returnValue = [];
+
+  void initMockData() {
+    executeCallCount = 0;
+    methodParameterMap.clear();
+    returnValue.clear();
+  }
+
+  /// [execute] 호출시
+  /// [executeCallCount] + 1
+  /// [methodParameterMap] 에 전달 받은 인자 저장
+  /// [returnValue] 반환
+  @override
+  Future<List<Feed>> execute({
+    required String email,
+    required DateTime? createdAt,
+    int? limit = 20,
+  }) async {
+    executeCallCount++;
+    methodParameterMap = {
+      'email': email,
+      'createdAt': createdAt,
+      'limit': limit,
+    };
+    return await Future.value(returnValue);
+  }
+}
