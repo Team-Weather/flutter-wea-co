159b4b867b13ba8dfd945c9d02420535fbbe08fc
KimDonghyeok <qoophon@gmail.com>
2024-05-06 02:19:10 +0900

    Test: MockFeedRepositoryImpl 수정

--- test/mock/data/feed/repository/mock_feed_repository_impl.dart
+++ test/mock/data/feed/repository/mock_feed_repository_impl.dart
@@ -8,6 +8,7 @@ class MockFeedRepositoryImpl implements FeedRepository {
   int getOotdFeedsListCallCount = 0;
   int getSearchFeedsCallCount = 0;
   String getFeedParamId = '';
+
   // 메서드 호출시 인자 확인을 위한 map
   final Map<String, dynamic> methodParameterMap = {};
   final List<Feed> _fakeFeedList = [];
@@ -60,37 +61,10 @@ class MockFeedRepositoryImpl implements FeedRepository {
   /// [_fakeFeedList]에서 조건에 맞는 피드 데이터를 찾아서 리스트로 반환
   /// 호출시 [getRecommendedFeedsCallCount] + 1
   @override
-  Future<List<Feed>> getRecommendedFeeds(
-      {int? seasonCode,
-      int? weatherCode,
-      int? minTemperature,
-      int? maxTemperature}) {
+  Future<List<Feed>> getRecommendedFeeds() {
     getRecommendedFeedsCallCount++;
-    methodParameterMap['seasonCode'] = seasonCode;
-    methodParameterMap['weatherCode'] = weatherCode;
-    methodParameterMap['minTemperature'] = minTemperature;
-    methodParameterMap['maxTemperature'] = maxTemperature;
 
     List<Feed> result = _fakeFeedList;
-
-    if (seasonCode != null) {
-      result =
-          result.where((element) => element.seasonCode == seasonCode).toList();
-    }
-
-    if (weatherCode != null) {
-      result = result
-          .where((element) => element.weather.code == weatherCode)
-          .toList();
-    }
-
-    if (minTemperature != null && maxTemperature != null) {
-      result = result
-          .where((element) =>
-              element.weather.temperature >= minTemperature &&
-              element.weather.temperature <= maxTemperature)
-          .toList();
-    }
 
     return Future.value(result);
   }
@@ -165,7 +139,7 @@ class MockFeedRepositoryImpl implements FeedRepository {
   /// [getOotdFeedsListCallCount] + 1
   /// [getOotdFeedsResult] 반환
   @override
-  Future<List<Feed>> getOotdFeedsList({required DateTime? createdAt}) async {
+  Future<List<Feed>> getOotdFeedsList({DateTime? createdAt}) async {
     getOotdFeedsListCallCount++;
     return getOotdFeedsResult == null ? [] : [getOotdFeedsResult!];
   }
