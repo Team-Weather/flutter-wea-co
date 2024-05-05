62b514d672baf60799e24dc2bf65136a32a4924d
gyubit <gyubros@gmail.com>
2024-05-05 19:50:47 +0900

    Chore: save_cropped_image_use_case의 test 코드 수정

--- test/mock/data/feed/repository/mock_image_repository_impl.dart
+++ test/mock/data/feed/repository/mock_image_repository_impl.dart
@@ -1,7 +1,4 @@
-import 'package:weaco/domain/feed/model/feed.dart';
 import 'package:weaco/domain/common/repository/image_repository.dart';
-import 'package:weaco/domain/location/model/location.dart';
-import 'package:weaco/domain/weather/model/weather.dart';
 
 class MockImageRepositoryImpl implements ImageRepository {
   @override
