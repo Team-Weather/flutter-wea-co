eae8304bb383c279f239d4bcf402903e4e9641a4
SeongJongHo <whdgh9595@gmail.com>
2024-05-06 00:43:00 +0900

    Modify: UserProfile 관련 파일명 변경 및 코드 병합

--- lib/domain/user/use_case/get_user_profile_use_case.dart
+++ lib/domain/user/use_case/get_user_profile_use_case.dart
@@ -1,14 +1,14 @@
 import 'package:weaco/domain/user/model/user_profile.dart';
 import 'package:weaco/domain/user/repository/user_profile_repository.dart';
 
-class GetDetailProfileUseCase {
+class GetUserProfileUseCase {
   final UserProfileRepository _userProfileRepository;
 
-  GetDetailProfileUseCase({
+  GetUserProfileUseCase({
     required UserProfileRepository userProfileRepository,
   }) : _userProfileRepository = userProfileRepository;
 
-  /// OOTD 상세 화면에 해당하는 유저 정보를 가져온다.
+  /// 해당하는 유저 정보를 가져온다.
   /// @return: 해당하는 email 이 없을 경우 null 을 반환
   Future<UserProfile?> execute({required String email}) async {
     return await _userProfileRepository.getUserProfile(email: email);
