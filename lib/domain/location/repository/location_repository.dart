67449801b4bcdc9825fd15b018849e882d816618
KimDonghyeok <qoophon@gmail.com>
2024-05-06 00:25:35 +0900

    Feat : LocationRepository 수정
    - getLocation() 파라미터 수정

--- lib/domain/location/repository/location_repository.dart
+++ lib/domain/location/repository/location_repository.dart
@@ -1,5 +1,8 @@
 import 'package:weaco/domain/location/model/location.dart';
 
 abstract interface class LocationRepository {
-  Future<Location?> getLocation({double? lat, double lng});
+  Future<Location?> getLocation({
+    required double lat,
+    required double lng,
+  });
 }
