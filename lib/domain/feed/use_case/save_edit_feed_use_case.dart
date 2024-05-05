db6f552fd55f58c78cd1067043eed3acf7e054f0
YunZi.Song <yoonji5809@gmail.com>
2024-05-05 14:02:53 +0900

    Feat: SaveEditFeedUseCase try-catch문 수정

--- lib/domain/feed/use_case/save_edit_feed_use_case.dart
+++ lib/domain/feed/use_case/save_edit_feed_use_case.dart
@@ -11,13 +11,6 @@ class SaveEditFeedUseCase {
   /// @param feed: 저장할 피드
   /// @return: 서버 응답 메세지. response 모델로 변경 예정.
   Future<bool> execute({required Feed feed}) async {
-    bool response = false;
-
-    try {
-      response = await _feedRepository.saveFeed(editedFeed: feed);
-    } on Exception catch (e) {
-      Exception(e);
-    }
-    return response;
+    return await _feedRepository.saveFeed(editedFeed: feed);
   }
 }
