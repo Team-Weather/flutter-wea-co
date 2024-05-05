77679c17e50493a7d102fec3550273a081ddaff8
KimDonghyeok <qoophon@gmail.com>
2024-05-05 20:29:26 +0900

    Test: SignInUserUseCase 테스트 코드 수정

--- test/domain/user/use_case/sign_in_user_use_case_test.dart
+++ test/domain/user/use_case/sign_in_user_use_case_test.dart
@@ -11,6 +11,10 @@ void main() {
   final SignInUserUseCase signInUserUseCase =
       SignInUserUseCase(userAuthRepository: mockUserAuthRepositoryImpl);
 
+  setUp(() {
+    mockUserAuthRepositoryImpl.initMockData();
+  });
+
   group('SignInUserUseCase 클래스', () {
     group('execute() 메소드는', () {
       test('UserAuthRepository.signIn() 메소드를 1회 호출한다.', () async {
@@ -20,9 +24,6 @@ void main() {
           email: 'qoophon@gmail.com',
           password: 'password',
         );
-
-        const expectReturnValue = true;
-        mockUserAuthRepositoryImpl.returnValue = expectReturnValue;
 
         await signInUserUseCase.execute(userAuth: expectParameter);
 
@@ -34,26 +35,45 @@ void main() {
           password: 'password',
         );
 
-        const expectReturnValue = true;
-        mockUserAuthRepositoryImpl.returnValue = expectReturnValue;
-
         await signInUserUseCase.execute(userAuth: expectParameter);
 
         expect(mockUserAuthRepositoryImpl.methodParameter, expectParameter);
       });
-      test('UserAuthRepository.signIn() 로 반환받은 인자를 그대로 반환한다. ', () async {
+      test('일치하는 유저가 없을시 false를 반환한다.', () async {
         final expectParameter = UserAuth(
           email: 'qoophon@gmail.com',
           password: 'password',
         );
 
-        const expectReturnValue = true;
-        mockUserAuthRepositoryImpl.returnValue = expectReturnValue;
+        mockUserAuthRepositoryImpl.addUserAuth(
+          UserAuth(
+            email: 'qoophon@gmail.com',
+            password: 'password1',
+          ),
+        );
 
         final actualReturnValue =
             await signInUserUseCase.execute(userAuth: expectParameter);
 
-        expect(actualReturnValue, expectReturnValue);
+        expect(actualReturnValue, false);
+      });
+      test('일치하는 유저가 있을 시 true를 반환한다.', () async {
+        final expectParameter = UserAuth(
+          email: 'qoophon@gmail.com',
+          password: 'password',
+        );
+
+        mockUserAuthRepositoryImpl.addUserAuth(
+          UserAuth(
+            email: 'qoophon@gmail.com',
+            password: 'password',
+          ),
+        );
+
+        final actualReturnValue =
+            await signInUserUseCase.execute(userAuth: expectParameter);
+
+        expect(actualReturnValue, true);
       });
     });
   });
