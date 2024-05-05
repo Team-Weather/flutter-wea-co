e691e6d0e7219c229f7782066a93cfc44546bd57
gyubit <gyubros@gmail.com>
2024-05-05 19:56:54 +0900

    Modify: file_repository 내 getCroppedImage 메서드의 파라미터 제거

--- lib/domain/common/file/repository/file_repository.dart
+++ lib/domain/common/file/repository/file_repository.dart
@@ -3,5 +3,5 @@ import 'dart:io';
 abstract interface class FileRepository {
   Future<bool> saveData({required List<int> data});
 
-  Future<File?> getCroppedImage({required List<int> data});
+  Future<File?> getCroppedImage();
 }
