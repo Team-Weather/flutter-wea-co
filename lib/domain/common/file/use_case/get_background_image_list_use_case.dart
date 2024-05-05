4a5a93b46b033f367420cc44aeec94e5ae230a7e
hoogom88 <ziziwhdgus@naver.com>
2024-05-06 01:29:13 +0900

    Refactor: GetHomeBackgroundImageListUseCase -> GetBackgroundImageListUseCase 이름 변경

--- lib/domain/common/file/use_case/get_background_image_list_use_case.dart
+++ lib/domain/common/file/use_case/get_background_image_list_use_case.dart
@@ -1,10 +1,10 @@
 import 'package:weaco/domain/common/file/repository/file_repository.dart';
 import 'package:weaco/domain/weather/model/weather_background_image.dart';
 
-class GetHomeBackgroundImageListUseCase {
+class GetBackgroundImageListUseCase {
   final FileRepository _fileRepository;
 
-  GetHomeBackgroundImageListUseCase({required FileRepository fileRepository})
+  GetBackgroundImageListUseCase({required FileRepository fileRepository})
       : _fileRepository = fileRepository;
 
   /// 날씨 화면의 배경 이미지를 가져오기 위한 Use Case
--- test/domain/common/file/use_case/get_background_image_list_use_case_test.dart
+++ test/domain/common/file/use_case/get_background_image_list_use_case_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:weaco/domain/common/file/use_case/get_home_background_image_list_use_case.dart';
+import 'package:weaco/domain/common/file/use_case/get_background_image_list_use_case.dart';
 import 'package:weaco/domain/weather/model/weather_background_image.dart';
 
 import '../../../../mock/data/common/file/repository/mock_file_repository_impl.dart';
@@ -7,8 +7,8 @@ import '../../../../mock/data/common/file/repository/mock_file_repository_impl.d
 void main() {
   group('GetHomeImageUseCase 클래스', () {
     final mockFileRepository = MockFileRepositoryImpl();
-    final GetHomeBackgroundImageListUseCase getHomeBackgroundImageListUseCase =
-        GetHomeBackgroundImageListUseCase(fileRepository: mockFileRepository);
+    final GetBackgroundImageListUseCase getHomeBackgroundImageListUseCase =
+        GetBackgroundImageListUseCase(fileRepository: mockFileRepository);
     setUp(() => mockFileRepository.initMockData());
     group('execute 메소드는', () {
       test(
