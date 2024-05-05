d115594dedfd529e7b6122b6f81e51da59cb6600
gyubit <gyubros@gmail.com>
2024-05-05 19:44:19 +0900

    Modify: save_cropped_image_use_case의 test용 mock 데이터 수정

--- test/mock/data/feed/repository/mock_image_repository_impl.dart
+++ test/mock/data/feed/repository/mock_image_repository_impl.dart
@@ -5,26 +5,7 @@ import 'package:weaco/domain/weather/model/weather.dart';
 
 class MockImageRepositoryImpl implements ImageRepository {
   @override
-  Future<Feed> saveCroppedImage(String imagePath) async {
-    return Feed(
-      id: '1',
-      imagePath: 'imagePath',
-      userEmail: 'test@email.com',
-      description: 'description',
-      weather: Weather(
-        temperature: 1.0,
-        timeTemperature: DateTime.now(),
-        code: 1,
-        createdAt: DateTime.now(),
-      ),
-      seasonCode: 1,
-      location: Location(
-        lat: 1.0,
-        lng: 1.0,
-        city: '서울시, 노원구',
-        createdAt: DateTime.now(),
-      ),
-      createdAt: DateTime.now(),
-    );
+  Future<bool> saveCroppedImage({required List<int> data}) {
+    return Future.value(true);
   }
 }
