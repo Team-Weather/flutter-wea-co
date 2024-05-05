bf533bd3a8976985b6c1b1dc5e67ea64225ce8ea
gyubit <gyubros@gmail.com>
2024-05-05 22:44:17 +0900

    Feat: Code update. User repository 내 email 중복 확인 기능 추가

--- lib/domain/user/repository/user_repository.dart
+++ lib/domain/user/repository/user_repository.dart
@@ -2,7 +2,10 @@ import 'package:weaco/domain/user/model/user_auth.dart';
 import 'package:weaco/domain/user/model/user_profile.dart';
 
 abstract interface class UserRepository {
-  Future<bool> signUp(UserAuth userAuth, UserProfile userProfile, String email);
+  Future<bool> signUp({required UserAuth userAuth, required UserProfile
+  userProfile});
+
+  Future<bool> isRegistered({required String email});
 
   Future<bool> logOut();
 }
