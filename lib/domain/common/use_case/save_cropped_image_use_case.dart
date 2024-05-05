515ebe7beb3fe9e419b7a6695504d10362071c0e
gyubit <gyubros@gmail.com>
2024-05-05 19:43:50 +0900

    Modify: save_cropped_image_use_case 메서드 수정

--- lib/domain/common/use_case/save_cropped_image_use_case.dart
+++ lib/domain/common/use_case/save_cropped_image_use_case.dart
@@ -1,15 +1,16 @@
 import 'package:weaco/domain/common/repository/image_repository.dart';
-import 'package:weaco/domain/feed/model/feed.dart';
 
 class SaveCroppedImageUseCase {
   final ImageRepository _imageRepository;
 
-  SaveCroppedImageUseCase(this._imageRepository);
+  const SaveCroppedImageUseCase({
+    required ImageRepository imageRepository,
+  }) : _imageRepository = imageRepository;
 
-  /// 레포지토리에 이미지 imagePath를 전달하여 해당 이미지를 저장한다.
-  /// @param imagePath: 이미지 경로
-  /// @return Feed: 피드
-  Future<Feed> execute(String imagePath) async {
-    return _imageRepository.saveCroppedImage(imagePath);
+  /// 이미지를 저장하는 함수
+  /// @param data: 이미지 데이터
+  /// @return: 이미지 저장 성공 여부
+  Future<bool> execute({required List<int> data}) async {
+    return await _imageRepository.saveCroppedImage(data: data);
   }
 }
