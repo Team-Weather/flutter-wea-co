8230a7caa3d90cd273e71cdcf3441a0904503f02
gyubit <gyubros@gmail.com>
2024-05-05 19:18:04 +0900

    Feat: User repository 파일 내sign_up_user_use_case를 위한 코드 추가

--- lib/domain/user/repository/user_repository.dart
+++ lib/domain/user/repository/user_repository.dart
@@ -1,1 +1,6 @@
-abstract interface class UserRepository {}
+import 'package:weaco/domain/user/model/user_auth.dart';
+import 'package:weaco/domain/user/model/user_profile.dart';
+
+abstract interface class UserRepository {
+  Future<bool> signUp(UserAuth userAuth, UserProfile userProfile, String email);
+}
