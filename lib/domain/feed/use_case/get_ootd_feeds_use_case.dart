351f78cbe4717da8c0e124021e81fed28ef374da
gyubit <gyubros@gmail.com>
2024-05-05 20:08:43 +0900

    Modify: 1. OOTD -> Ootd 2. try - catch 문 제거

--- lib/domain/feed/use_case/get_ootd_feeds_use_case.dart
+++ lib/domain/feed/use_case/get_ootd_feeds_use_case.dart
@@ -2,23 +2,19 @@ import 'package:weaco/domain/feed/model/feed.dart';
 import '../repository/feed_repository.dart';
 
 /// OOTD 피드 페이지에서 유저의 피드 목록을 보여주기 위한 Use Case
-class GetOOTDFeedsUseCase {
+class GetOotdFeedsUseCase {
   final FeedRepository _feedRepository;
 
-  GetOOTDFeedsUseCase({required FeedRepository feedRepository})
+  GetOotdFeedsUseCase({required FeedRepository feedRepository})
       : _feedRepository = feedRepository;
 
   Future<List<Feed>> execute({
     required DateTime? createdAt,
   }) async {
     List<Feed> feedList = [];
-    try {
-      feedList = await _feedRepository.getOOTDFeedsList(
-        createdAt: createdAt,
-      );
-    } catch (e) {
-      Exception(e);
-    }
+    feedList = await _feedRepository.getOotdFeedsList(
+      createdAt: createdAt,
+    );
     return feedList;
   }
 }
