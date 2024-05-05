1ece2364edbc7b15c1f1eda676def4bc9d26d87a
KimDonghyeok <qoophon@gmail.com>
2024-05-06 00:54:12 +0900

    Feat : GetHomeLocationUseCase 수정
    - get_home_location_use_case.dart 에서 get_location_from_coordinate_use_case.dart 로 파일 이름 변경
    - GetHomeLocationUseCase 에서 GetLocationFromCoordinateUseCase 로 클래스 명 변경

--- lib/domain/location/use_case/get_location_from_coordinate_use_case.dart
+++ lib/domain/location/use_case/get_location_from_coordinate_use_case.dart
@@ -1,18 +1,21 @@
 import 'package:weaco/domain/location/model/location.dart';
 import 'package:weaco/domain/location/repository/location_repository.dart';
 
-class GetHomeLocationUseCase {
+class GetLocationFromCoordinateUseCase {
   final LocationRepository _locationRepository;
 
-  GetHomeLocationUseCase({
+  GetLocationFromCoordinateUseCase({
     required LocationRepository locationRepository,
   }) : _locationRepository = locationRepository;
 
-  /// 홈 화면의 위치 정보를 가져오기 위한 Use Case
-  /// [lat] 위도
-  /// [lng] 경도
-  /// return [Location] 위치 정보
-  Future<Location?> execute({required double lat, required double lng}) async {
+  /// 위도, 경도로 위치 정보를 가져오기 위한 Use Case
+  /// @param [lat] 위도
+  /// @param [lng] 경도
+  /// @return [Location] 위치 정보
+  Future<Location?> execute({
+    required double lat,
+    required double lng,
+  }) async {
     return await _locationRepository.getLocation(lat: lat, lng: lng);
   }
 }
