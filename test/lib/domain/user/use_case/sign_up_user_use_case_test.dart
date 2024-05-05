26d58b47a0fd4c4f2c389374b40142506d33192c
gyubit <gyubros@gmail.com>
2024-05-05 22:54:19 +0900

    Test: Code update. sign_up_user_use_case의 Test 코드 업데이트

--- test/lib/domain/user/use_case/sign_up_user_use_case_test.dart
+++ test/lib/domain/user/use_case/sign_up_user_use_case_test.dart
@@ -6,31 +6,93 @@ import '../../../../mock/data/user/repository/mock_user_repository_impl.dart';
 
 void main() {
   group('SignUpUserUseCase 클래스', () {
+    final mockUserRepository = MockUserRepositoryImpl();
+    final SignUpUserUseCase signUpUserUseCase =
+        SignUpUserUseCase(userRepository: mockUserRepository);
     group('execute 메서드는', () {
-      test('UserRepository.signUpUser()를 한번 호출한다.', () async {
+      test('UserRepository.signUpUser()를 호출하여 회원가입에 성공하면 true를 반환한다.',
+          () async {
+        MockUserRepositoryImpl().initMockData();
+
         // Given
-        UserAuth userAuth =
-            UserAuth(email: 'test@email.com', password: 'password');
-        final userEmail = userAuth.email;
-        UserProfile userProfile = UserProfile(
-          email: userEmail,
+        const existingEmail = 'existingUser@email.com';
+        const newEmail = 'newUser@email.com';
+        const newEmailDuplicated = 'existingUser@email.com';
+        final existingUserAuth =
+            UserAuth(email: existingEmail, password: 'password');
+        final existingUserProfile = UserProfile(
+          email: existingUserAuth.email,
           nickname: 'nickname',
           gender: 1,
-          profileImagePath: '',
+          profileImagePath: 'profileImagePath',
           feedCount: 0,
-          createdAt: DateTime.parse('2024-05-04'),
-          deletedAt: DateTime.parse('2024-05-04'),
+          createdAt: DateTime.parse('2024-05-01'),
+        );
+        final newUserAuth = UserAuth(email: newEmail, password: 'password');
+        final newUserProfile = UserProfile(
+          email: newUserAuth.email,
+          nickname: 'nickname',
+          gender: 1,
+          profileImagePath: 'profileImagePath',
+          feedCount: 0,
+          createdAt: DateTime.parse('2024-05-05'),
+        );
+        final newUserAuth2 =
+            UserAuth(email: newEmailDuplicated, password: 'password');
+
+        // When
+        signUpUserUseCase.execute(
+            userAuth: existingUserAuth, userProfile: existingUserProfile);
+        if (!await MockUserRepositoryImpl()
+            .isRegistered(email: newUserAuth.email)) {
+          bool success = await MockUserRepositoryImpl()
+              .signUp(userAuth: newUserAuth, userProfile: newUserProfile);
+
+          // Then
+          expect(success, true);
+        }
+      });
+
+      test('UserRepository.signUpUser()를 호출하여 회원가입에 실패하면 false를 반환한다.',
+          () async {
+        MockUserRepositoryImpl().initMockData();
+
+        // Given
+        const existingEmail = 'existingUser@email.com';
+        const newEmailDuplicated = 'existingUser@email.com';
+        final existingUserAuth =
+            UserAuth(email: existingEmail, password: 'password');
+        final existingUserProfile = UserProfile(
+          email: existingUserAuth.email,
+          nickname: 'nickname',
+          gender: 1,
+          profileImagePath: 'profileImagePath',
+          feedCount: 0,
+          createdAt: DateTime.parse('2024-05-01'),
+        );
+        final newUserAuth =
+            UserAuth(email: newEmailDuplicated, password: 'password');
+        final newUserProfileDuplicated = UserProfile(
+          email: newUserAuth.email,
+          nickname: 'nickname',
+          gender: 1,
+          profileImagePath: 'profileImagePath',
+          feedCount: 0,
+          createdAt: DateTime.parse('2024-05-05'),
         );
 
         // When
-        final signUpUserUseCase =
-            SignUpUserUseCase(userRepository: MockUserRepositoryImpl());
-        await signUpUserUseCase.execute(userAuth, userProfile, userEmail);
+        signUpUserUseCase.execute(
+            userAuth: existingUserAuth, userProfile: existingUserProfile);
 
-        // Then
-        expect(
-            MockUserRepositoryImpl().signUp(userAuth, userProfile, userEmail),
-            completion(true));
+        if (!await MockUserRepositoryImpl()
+            .isRegistered(email: newUserAuth.email)) {
+          bool fail = await MockUserRepositoryImpl().signUp(
+              userAuth: newUserAuth, userProfile: newUserProfileDuplicated);
+
+          // Then
+          expect(fail, true);
+        }
       });
     });
   });
