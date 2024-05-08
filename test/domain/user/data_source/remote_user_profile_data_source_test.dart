85f14029b0cf5ba98282a97582b4ad4d23d6253e
nala0897@naver.com <nala0897@naver.com>
2024-05-08 18:30:42 +0900

    Test: MockFirebaseAuthService 생성 및 RemoteUserProfileDataSource 테스트 코드 수정

--- test/domain/user/data_source/remote_user_profile_data_source_test.dart
+++ test/domain/user/data_source/remote_user_profile_data_source_test.dart
@@ -4,14 +4,22 @@ import 'package:weaco/data/user/data_source/remote_user_profile_data_source.dart
 import 'package:weaco/data/user/data_source/remote_user_profile_date_source_impl.dart';
 import 'package:weaco/domain/user/model/user_profile.dart';
 
-void main() {
+import '../../../mock/core/firebase/mock_firebase_auth_service.dart';
+
+void main() async {
+
   final instance = FakeFirebaseFirestore();
+  final firebaseService = MockFirebaseAuthService();
+
   final RemoteUserProfileDataSource dataSource =
-      RemoteUserProfileDataSourceImpl(firestore: instance);
+      RemoteUserProfileDataSourceImpl(firestore: instance, firebaseService: firebaseService);
 
   group(
     'RemoteUserProfileDataSource 클래스',
     () {
+
+      setUp(() => firebaseService.initMockData());
+
       test('saveUserProfile()은 firebase storage에 파라미터로 받은 유저 프로필 정보를 저장한다.',
           () async {
         // Given
@@ -66,8 +74,39 @@ void main() {
         expect(res, expectProfile);
       });
 
+      test('getUserProfile() email 이 null 일 경우, 현재 유저 프로필 정보를 반환한다.', () async {
+        // Given
+        await instance.collection('user_profiles').add({
+          'created_at': '2024-05-08 02:27:00',
+          'deleted_at': null,
+          'email': firebaseService.user.email,
+          'feed_count': 0,
+          'gender': 1,
+          'nickname': '호구몬',
+          'profile_image_path':
+              'https://health.chosun.com/site/data/img_dir/2024/01/22/2024012201607_0.jpg'
+        });
+
+        final expectProfile = UserProfile(
+          email: 'bob@somedomain.com',
+          nickname: '호구몬',
+          gender: 1,
+          profileImagePath:
+              'https://health.chosun.com/site/data/img_dir/2024/01/22/2024012201607_0.jpg',
+          feedCount: 0,
+          createdAt: DateTime.parse('2024-05-08 02:27:00'),
+          deletedAt: null,
+        );
+
+        // When
+        final result = await dataSource.getUserProfile();
+
+        // Then
+        expect(result, expectProfile);
+      },);
+
       test(
-        'updateUserProfile()은 firebase storage에서 파라미터로 받은 프로필로 동일한 유저 프로필 정보를 업데이트한다.',
+        'updateUserProfile()은 userProfile 이 null 일 경우, 현재 유저 프로필 정보를 업데이트한다.',
         () async {
           // Given
           await instance.collection('user_profiles').add({
@@ -119,6 +158,30 @@ void main() {
           // When
           final bool res =
               await dataSource.removeUserProfile(email: targetEmail);
+
+          // Then
+          expect(res, true);
+        },
+      );
+
+      test(
+        'removeUserProfile()은 userProfile 이 null 일 경우, 현재 유저 프로필 정보를 삭제한다.',
+            () async {
+          // Given
+          await instance.collection('user_profiles').add({
+            'created_at': '2024-05-08 02:27:00',
+            'deleted_at': null,
+            'email': firebaseService.user.email,
+            'feed_count': 0,
+            'gender': 1,
+            'nickname': '호구몬',
+            'profile_image_path':
+            'https://health.chosun.com/site/data/img_dir/2024/01/22/2024012201607_0.jpg'
+          });
+
+          // When
+          final bool res =
+          await dataSource.removeUserProfile();
 
           // Then
           expect(res, true);
