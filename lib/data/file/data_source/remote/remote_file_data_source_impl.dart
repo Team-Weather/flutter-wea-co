c2e0f27f5b21df3c9645b84162e0791f0d4ea673
KimDonghyeok <qoophon@gmail.com>
2024-05-08 12:41:20 +0900

    Feat: RemoteFileDataSourceImpl 수정

--- lib/data/file/data_source/remote/remote_file_data_source_impl.dart
+++ lib/data/file/data_source/remote/remote_file_data_source_impl.dart
@@ -6,11 +6,15 @@ import 'package:weaco/data/file/data_source/remote/remote_file_data_source.dart'
 class RemoteFileDataSourceImpl implements RemoteFileDataSource {
   final FirebaseStorage _firebaseStorage;
 
-  RemoteFileDataSourceImpl() : _firebaseStorage = FirebaseStorage.instance;
+  RemoteFileDataSourceImpl({required FirebaseStorage firebaseStorage})
+      : _firebaseStorage = firebaseStorage;
 
   @override
-  Future<String?> saveImage(File image) async {
-    final feedImageRef = _firebaseStorage.ref().child('feed_images');
+  Future<String> saveImage({required File image}) async {
+    final splitImagePath = image.path.split('/');
+    final imageName = splitImagePath[splitImagePath.length - 1];
+
+    final feedImageRef = _firebaseStorage.ref().child('feed_images/$imageName');
     await feedImageRef.putFile(image);
 
     return await feedImageRef.getDownloadURL();
