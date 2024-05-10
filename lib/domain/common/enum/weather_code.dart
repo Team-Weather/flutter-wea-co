bbec4ddfec193f5ff0dbe86d414a483365e57d2a
hoogom88 <ziziwhdgus@naver.com>
2024-05-10 20:44:31 +0900

    Feat: WeatherCode, description 필드 추가

--- lib/domain/common/enum/weather_code.dart
+++ lib/domain/common/enum/weather_code.dart
@@ -1,26 +1,27 @@
 enum WeatherCode {
-  noData(value: 0, iconPath: ''), // 맑음
-  clearSky(value: 1, iconPath: ''), // 맑음
-  partlyCloudy(value: 2, iconPath: ''), // 조금 흐림
-  fog(value: 3, iconPath: ''), // 안개
-  drizzle(value: 4, iconPath: ''), // 이슬비
-  freezingDrizzle(value: 5, iconPath: ''), // 얼어 붙은 이슬비
-  rain(value: 6, iconPath: ''), // 비
-  rainShower(value: 7, iconPath: ''), // 소나기
-  freezingRain(value: 8, iconPath: ''), // 얼어 붙은 비
-  snow(value: 9, iconPath: ''), // 눈
-  snowGrain(value: 10, iconPath: ''), // 싸락눈
-  snowShower(value: 11, iconPath: ''), // 소낙눈
-  thunderStorm(value: 12, iconPath: ''), // 뇌우
-  thunderStormHail(value: 13, iconPath: ''); // 우박을 동반한 뇌우
+  noData(value: 0, iconPath: '', description: '-'), // 맑음
+  clearSky(value: 1, iconPath: '', description: '맑음'), // 맑음
+  partlyCloudy(value: 2, iconPath: '', description: '조금 흐림'), // 조금 흐림
+  fog(value: 3, iconPath: '', description: '안개'), // 안개
+  drizzle(value: 4, iconPath: '', description: '이슬비'), // 이슬비
+  freezingDrizzle(
+      value: 5, iconPath: '', description: '얼어 붙은 이슬비'), // 얼어 붙은 이슬비
+  rain(value: 6, iconPath: '', description: '비'), // 비
+  rainShower(value: 7, iconPath: '', description: '소나기'), // 소나기
+  freezingRain(value: 8, iconPath: '', description: '얼어 붙은 비'), // 얼어 붙은 비
+  snow(value: 9, iconPath: '', description: '눈'), // 눈
+  snowGrain(value: 10, iconPath: '', description: '싸락눈'), // 싸락눈
+  snowShower(value: 11, iconPath: '', description: '소낙눈'), // 소낙눈
+  thunderStorm(value: 12, iconPath: '', description: '뇌우'), // 뇌우
+  thunderStormHail(
+      value: 13, iconPath: '', description: '우박을 동반한 뇌우'); // 우박을 동반한 뇌우
 
   final int value;
   final String iconPath;
+  final String description;
 
-  const WeatherCode({
-    required this.value,
-    required this.iconPath,
-  });
+  const WeatherCode(
+      {required this.value, required this.iconPath, required this.description});
 
   static WeatherCode fromValue(int value) {
     return switch (value) {
