45dd2c48087f8210c4799aedc6ff354c3b28d5d2
gyubit <gyubros@gmail.com>
2024-05-11 22:04:29 +0900

    Feat: router_static 클래스 생성

---
+++ lib/core/go_router/router_static.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:weaco/core/enum/router_path.dart';
+import 'package:weaco/core/go_router/router.dart';
+
+class RouterStatic {
+  static void goToDefault(BuildContext context) {
+    router.go(RouterPath.defaultPage.path);
+  }
+
+  static void goToHome(BuildContext context) {
+    router.go(RouterPath.home.path);
+  }
+
+  static void goToSignUp(BuildContext context) {
+    router.go(RouterPath.signUp.path);
+  }
+
+  static void goToSignIn(BuildContext context) {
+    router.go(RouterPath.signIn.path);
+  }
+
+  static void goToDialog(BuildContext context) {
+    router.go(RouterPath.dialog.path);
+  }
+
+  static void goToAppSetting(BuildContext context) {
+    router.go(RouterPath.appSetting.path);
+  }
+
+  static void goToMyPage(BuildContext context) {
+    router.go(RouterPath.myPage.path);
+  }
+
+  static void goToUserPage(BuildContext context) {
+    router.go(RouterPath.userPage.path);
+  }
+
+  static void goToOotdSearch(BuildContext context) {
+    router.go(RouterPath.ootdSearch.path);
+  }
+
+  static void goToOotdFeed(BuildContext context) {
+    router.go(RouterPath.ootdFeed.path);
+  }
+
+  static void goToOotdDetail(BuildContext context) {
+    router.go(RouterPath.ootdDetail.path);
+  }
+
+  static void goToCamera(BuildContext context) {
+    router.go(RouterPath.camera.path);
+  }
+
+  static void goToPictureCrop(BuildContext context) {
+    router.go(RouterPath.pictureCrop.path);
+  }
+
+  static void goToOotdPost(BuildContext context) {
+    router.go(RouterPath.ootdPost.path);
+  }
+}
