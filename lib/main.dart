964307cf42d4ba03fb45433fe406e6cc1d313e31
SeongJongHo <whdgh9595@gmail.com>
2024-05-12 22:47:07 +0900

    Feat: SignInScreen main에 적용

--- lib/main.dart
+++ lib/main.dart
@@ -72,7 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
               child: const Text('Go to SignUpScreen'),
             ),
             TextButton(
-              onPressed: () {},
+              onPressed: () => RouterStatic.goToSignIn(context),
               child: const Text('Go to SignInScreen'),
             ),
             TextButton(
