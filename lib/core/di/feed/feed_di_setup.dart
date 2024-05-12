fe18c1f67ae6534fb8d0eb1a6feccc955048f3f2
SeongJongHo <whdgh9595@gmail.com>
2024-05-12 21:15:00 +0900

    Feat: FeedRepository, OotdFeedRepository getIt 추가 및 usecase getIt 제네릭에 타입 추가

--- lib/core/di/feed/feed_di_setup.dart
+++ lib/core/di/feed/feed_di_setup.dart
@@ -2,6 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:weaco/core/di/di_setup.dart';
 import 'package:weaco/data/feed/data_source/remote_feed_data_source.dart';
 import 'package:weaco/data/feed/data_source/remote_feed_data_source_impl.dart';
+import 'package:weaco/data/feed/repository/feed_repository_impl.dart';
+import 'package:weaco/data/feed/repository/ootd_feed_repository_impl.dart';
+import 'package:weaco/domain/feed/repository/feed_repository.dart';
+import 'package:weaco/domain/feed/repository/ootd_feed_repository.dart';
 import 'package:weaco/domain/feed/use_case/get_detail_feed_detail_use_case.dart';
 import 'package:weaco/domain/feed/use_case/get_my_page_feeds_use_case.dart';
 import 'package:weaco/domain/feed/use_case/get_ootd_feeds_use_case.dart';
@@ -10,6 +14,8 @@ import 'package:weaco/domain/feed/use_case/get_search_feeds_use_case.dart';
 import 'package:weaco/domain/feed/use_case/get_user_page_feeds_use_case.dart';
 import 'package:weaco/domain/feed/use_case/remove_my_page_feed_use_case.dart';
 import 'package:weaco/domain/feed/use_case/save_edit_feed_use_case.dart';
+import 'package:weaco/domain/file/repository/file_repository.dart';
+import 'package:weaco/domain/user/repository/user_profile_repository.dart';
 
 void feedDiSetup() {
   // DataSource
@@ -17,23 +23,28 @@ void feedDiSetup() {
       () => RemoteFeedDataSourceImpl(fireStore: getIt<FirebaseFirestore>()));
 
   // Repository
+  getIt.registerLazySingleton<FeedRepository>(() =>
+      FeedRepositoryImpl(remoteFeedDataSource: getIt<RemoteFeedDataSource>()));
+  getIt.registerLazySingleton<OotdFeedRepository>(() => OotdFeedRepositoryImpl(
+      fileRepository: getIt<FileRepository>(),
+      feedRepository: getIt<FeedRepository>(),
+      userProfileRepository: getIt<UserProfileRepository>()));
 
   // UseCase
-  ///[feedRepository] merge되면 밑의 getIt()에 제네릭으로 타입 주입
-  getIt.registerLazySingleton<GetDetailFeedDetailUseCase>(
-      () => GetDetailFeedDetailUseCase(feedRepository: getIt()));
+  getIt.registerLazySingleton<GetDetailFeedDetailUseCase>(() =>
+      GetDetailFeedDetailUseCase(feedRepository: getIt<FeedRepository>()));
   getIt.registerLazySingleton<GetMyPageFeedsUseCase>(
-      () => GetMyPageFeedsUseCase(feedRepository: getIt()));
+      () => GetMyPageFeedsUseCase(feedRepository: getIt<FeedRepository>()));
   getIt.registerLazySingleton<GetOotdFeedsUseCase>(
-      () => GetOotdFeedsUseCase(feedRepository: getIt()));
-  getIt.registerLazySingleton<GetRecommendedFeedsUseCase>(
-      () => GetRecommendedFeedsUseCase(feedRepository: getIt()));
+      () => GetOotdFeedsUseCase(feedRepository: getIt<FeedRepository>()));
+  getIt.registerLazySingleton<GetRecommendedFeedsUseCase>(() =>
+      GetRecommendedFeedsUseCase(feedRepository: getIt<FeedRepository>()));
   getIt.registerLazySingleton<GetSearchFeedsUseCase>(
-      () => GetSearchFeedsUseCase(feedRepository: getIt()));
+      () => GetSearchFeedsUseCase(feedRepository: getIt<FeedRepository>()));
   getIt.registerLazySingleton<GetUserPageFeedsUseCase>(
-      () => GetUserPageFeedsUseCase(feedRepository: getIt()));
+      () => GetUserPageFeedsUseCase(feedRepository: getIt<FeedRepository>()));
   getIt.registerLazySingleton<RemoveMyPageFeedUseCase>(
-      () => RemoveMyPageFeedUseCase(feedRepository: getIt()));
+      () => RemoveMyPageFeedUseCase(feedRepository: getIt<FeedRepository>()));
   getIt.registerLazySingleton<SaveEditFeedUseCase>(
-      () => SaveEditFeedUseCase(feedRepository: getIt()));
+      () => SaveEditFeedUseCase(feedRepository: getIt<FeedRepository>()));
 }
