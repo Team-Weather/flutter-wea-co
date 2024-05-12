e805ea4720293df6f0d539d9e31d417e7c5823c1
gyubit <gyubros@gmail.com>
2024-05-13 01:39:15 +0900

    FEAT: main.dart 내 1.bottom_navigation_widget 연결 2.floatingActionButton 구현 3.구동 확인을 위한 main.dart 코드 업데이트

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:weaco/core/go_router/router.dart';
 import 'package:weaco/core/go_router/router_static.dart';
+import 'package:weaco/presentation/home/home_screen.dart';
+import 'package:weaco/presentation/navigation_bar/bottom_navigation_widget.dart';
 import 'firebase_options.dart';
 
 late Box dataBox;
@@ -18,15 +22,23 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xffF2C347)),
+        primaryColor: const Color(0xffF2C347),
+        canvasColor: Colors.white,
+        scaffoldBackgroundColor: const Color(0xffF5F5F5),
         useMaterial3: true,
       ),
       routerConfig: router,
@@ -44,92 +56,106 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _currentIndex = 0;
+  bool isPressingFloatingActionButton = false;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final List<Widget> pages = [
+    const HomeScreen(),
+    const HomeScreen(),
+    Container(),
+    const HomeScreen(),
+    const HomeScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
+    double deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextButton(
+      body: Column(
+        children: [
+          TextButton(
               onPressed: () => RouterStatic.goToHome(context),
-              child: const Text('Go to HomeScreen'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('Go to SignUpScreen'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('Go to SignInScreen'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('Go to DialogScreen'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('Go to AppSettingScreen'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('Go to MyPageScreen'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('Go to UserPageScreen'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('Go to OotdSearchScreen'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('Go to OotdFeedScreen'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('Go to OotdDetailScreen'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('Go to CameraScreen'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('Go to PictureCropScreen'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('Go to OotdPostScreen'),
-            ),
-            const Spacer(),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+              child: const Text('Go to HomeScreen')),
+          pages[_currentIndex],
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Stack(
+        children: [
+          if (!isPressingFloatingActionButton)
+            SizedBox(
+              width: 72,
+              height: 72,
+              child: FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    isPressingFloatingActionButton = true;
+                  });
+                  Future.delayed(const Duration(milliseconds: 2000), () {
+                    setState(() {
+                      isPressingFloatingActionButton = false;
+                    });
+                  });
+                },
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                  side: BorderSide(
+                      color: Theme.of(context).primaryColor,
+                      width: deviceWidth * 0.005),
+                ),
+                backgroundColor: Theme.of(context).canvasColor,
+                child: const Icon(
+                  Icons.add,
+                  color: Color(0xffF2C347),
+                  size: 40,
+                ),
+              ),
+            ),
+          if (isPressingFloatingActionButton)
+            SizedBox(
+              width: 128,
+              height: 72,
+              child: FloatingActionButton(
+                onPressed: () {},
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                  side: BorderSide(
+                      color: Theme.of(context).primaryColor,
+                      width: deviceWidth * 0.005),
+                ),
+                backgroundColor: Theme.of(context).canvasColor,
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(
+                      Icons.camera_alt_rounded,
+                      color: Color(0xffF2C347),
+                      size: 40,
+                    ),
+                    Icon(
+                      Icons.photo_album_outlined,
+                      color: Color(0xffF2C347),
+                      size: 40,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationWidget(
+        currentIndex: _currentIndex,
+        onTap: (int value) {
+          setState(() {
+            _currentIndex = value;
+          });
+        },
       ),
     );
   }
