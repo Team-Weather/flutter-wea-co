4ab5c7d0095c06393af5e1ab0f89346304179ebd
SeongJongHo <whdgh9595@gmail.com>
2024-05-13 12:43:27 +0900

    Feat: 누락된 fileDiSetup 호출

--- lib/core/di/di_setup.dart
+++ lib/core/di/di_setup.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:weaco/core/di/common/common_di_setup.dart';
 import 'package:weaco/core/di/feed/feed_di_setup.dart';
+import 'package:weaco/core/di/file/file_di_setup.dart';
 import 'package:weaco/core/di/location/location_di_setup.dart';
 import 'package:weaco/core/di/user/user_di_setup.dart';
 import 'package:weaco/core/di/weather/weather_di_setup.dart';
@@ -9,6 +10,7 @@ final getIt = GetIt.instance;
 
 /// DI 설정
 /// - [commonDiSetup]: 공통 DI 설정
+/// - [fileDiSetup]: 파일 DI 설정
 /// - [userDiSetup]: 사용자 DI 설정
 /// - [locationDiSetup]: 위치 DI 설정
 /// - [feedDiSetup]: 피드 DI 설정
@@ -16,8 +18,9 @@ final getIt = GetIt.instance;
 ///  common -> user -> location -> feed -> weather 순으로 DI 설정 중요
 ///  각각 먼저 설정된 DI가 다음 DI에 영향을 줄 수 있기 때문
 /// - [setup]: 모든 DI 설정
-void setup() {
+void diSetup() {
   commonDiSetup();
+  fileDiSetup();
   userDiSetup();
   locationDiSetup();
   feedDiSetup();
