cd1ede1f6516e947474dafad8e467fabd2858d51
nala0897@naver.com <nala0897@naver.com>
2024-05-16 02:10:44 +0900

    Feat: FutureBuilder 제거, viewModel 상태 코드 생성

--- lib/presentation/ootd_post/picture_crop/picture_crop_screen.dart
+++ lib/presentation/ootd_post/picture_crop/picture_crop_screen.dart
@@ -1,10 +1,9 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:provider/provider.dart';
-import 'package:weaco/core/enum/router_path.dart';
+import 'package:weaco/core/go_router/router_static.dart';
 import 'package:weaco/presentation/ootd_post/picture_crop/picutre_crop_view_model.dart';
 
 class PictureCropScreen extends StatefulWidget {
@@ -15,7 +14,6 @@ class PictureCropScreen extends StatefulWidget {
 }
 
 class _PictureCropScreenState extends State<PictureCropScreen> {
-  late Future<void> _cropImageFuture;
   CroppedFile? _croppedFile;
   final String samplePath =
       '/data/data/team.weather.weaco/cache/0ddabc12-9269-428b-8123-b09d1230c5a62983180504067444417.jpg';
@@ -23,26 +21,12 @@ class _PictureCropScreenState extends State<PictureCropScreen> {
   @override
   void initState() {
     super.initState();
-    _cropImageFuture = _cropImage(samplePath);
+    _cropImage(samplePath);
   }
 
   @override
   Widget build(BuildContext context) {
-    final viewModel = context.watch<PictureCropViewModel>();
-
-    return FutureBuilder(
-      future: _cropImageFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        } else {
-          _cropResult(samplePath, viewModel);
-          return const Scaffold(
-            body: SizedBox(),
-          );
-        }
-      },
-    );
+    return const Scaffold();
   }
 
   Future<void> _cropImage(String samplePath) async {
@@ -67,29 +51,33 @@ class _PictureCropScreenState extends State<PictureCropScreen> {
 
     if (croppedFile != null) {
       _croppedFile = croppedFile;
+
+      _cropResult(samplePath);
     }
   }
 
-  void _cropResult(String samplePath, PictureCropViewModel viewModel) {
+  void _cropResult(String samplePath) async {
+    final PictureCropViewModel viewModel = context.read<PictureCropViewModel>();
     if (_croppedFile == null) {
       return;
     } else {
-      viewModel.saveOriginImage(file: File(samplePath));
+      await viewModel.saveOriginImage(file: File(samplePath));
 
-      viewModel.saveCroppedImage(
-        file: File(_croppedFile!.path),
-        callback: (result) {
-          if (result) {
-            context.push(RouterPath.ootdPost.path);
-          } else {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('잠시 후 다시 시도해 주시기 바랍니다.'),
-              ),
-            );
-          }
-        },
-      );
+      await viewModel.saveCroppedImage(file: File(_croppedFile!.path));
+
+      if (viewModel.status.isSuccess) {
+        if (mounted) {
+          RouterStatic.goToOotdPost(context);
+        }
+      } else if (viewModel.status.isError) {
+        if (mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('잠시 후 다시 시도해 주시기 바랍니다.'),
+            ),
+          );
+        }
+      }
     }
   }
 }
--- lib/presentation/ootd_post/picture_crop/picutre_crop_view_model.dart
+++ lib/presentation/ootd_post/picture_crop/picutre_crop_view_model.dart
@@ -3,26 +3,43 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:weaco/domain/file/use_case/save_image_use_case.dart';
 
+enum PictureCropSaveStatus {
+  idle,
+  success,
+  error;
+
+  bool get isIdle => this == PictureCropSaveStatus.idle;
+
+  bool get isSuccess => this == PictureCropSaveStatus.success;
+
+  bool get isError => this == PictureCropSaveStatus.error;
+}
+
 class PictureCropViewModel with ChangeNotifier {
   final SaveImageUseCase _saveImageUseCase;
+  PictureCropSaveStatus _status = PictureCropSaveStatus.idle;
 
   PictureCropViewModel({
     required SaveImageUseCase saveImageUseCase,
-  })  : _saveImageUseCase = saveImageUseCase;
+  }) : _saveImageUseCase = saveImageUseCase;
 
   /// 원본 이미지 저장
-  void saveOriginImage({required File file}) async {
+  Future<void> saveOriginImage({required File file}) async {
     await _saveImageUseCase.execute(isOrigin: true, file: file);
   }
 
   /// 크롭 이미지 저장
-  void saveCroppedImage({
-    required File file,
-    required Function(bool) callback,
-  }) async {
+  Future<void> saveCroppedImage({required File file}) async {
     final bool result =
         await _saveImageUseCase.execute(isOrigin: false, file: file);
 
-    callback(result);
+    if (result) {
+      _status = PictureCropSaveStatus.success;
+    } else {
+      _status = PictureCropSaveStatus.error;
+    }
+    notifyListeners();
   }
+
+  PictureCropSaveStatus get status => _status;
 }
