676620a15a3e5010d37f28d2a4700631a2ba721e
SeongJongHo <whdgh9595@gmail.com>
2024-05-16 06:07:54 +0900

    Feat: ExceptionMessageHandler 문구 변경

--- lib/core/exception/handler/exception_message_handler.dart
+++ lib/core/exception/handler/exception_message_handler.dart
@@ -21,13 +21,14 @@ class ExceptionMessageHandler {
     log(e.message ?? 'Dio Error');
 
     return switch (e.type) {
-      DioExceptionType.connectionTimeout => '연결 시간 초과',
-      DioExceptionType.sendTimeout => '전송 시간 초과',
-      DioExceptionType.receiveTimeout => '수신 시간 초과',
-      DioExceptionType.badCertificate => '잘못된 인증서',
-      DioExceptionType.badResponse => '잘못된 응답',
+      DioExceptionType.connectionTimeout ||
+      DioExceptionType.connectionError ||
+      DioExceptionType.sendTimeout ||
+      DioExceptionType.receiveTimeout =>
+        '네트워크가 불안정 합니다.',
+      DioExceptionType.badCertificate => '인증되지 않는 유저입니다.',
+      DioExceptionType.badResponse => '잘못된 요청입니다.',
       DioExceptionType.cancel => '요청이 취소되었습니다',
-      DioExceptionType.connectionError => 'connection error',
       DioExceptionType.unknown => '알 수 없는 오류',
     };
   }
@@ -42,6 +43,69 @@ class ExceptionMessageHandler {
     };
   }
 
+  /// Firebase Auth Exception 분기 및 다이얼로그 컨텐츠에 들어갈 메세지 반환
+  String _handleAuthException(FirebaseException e) {
+    log(e.message ?? 'Auth Error');
+
+    return switch (e.code) {
+      'user-not-found' => '존재하지 않는 유저입니다.',
+      'wrong-password' => '비밀번호가 맞지 않습니다.',
+      'email-already-in-use' => '이미 가입된 이메일입니다.',
+      'invalid-email' => '이메일 형식이 올바르지 못합니다.',
+      'operation-not-allowed' => '허용되지 않는 요청입니다.',
+      'weak-password' => '비밀번호 형식이 안전하지 않습니다.',
+      'network-request-failed' => '네트워크가 불안정 합니다.',
+      'too-many-requests' => '요청이 너무 많습니다.',
+      'user-disabled' => '이미 탈퇴한 회원입니다.',
+      _ => 'Auth Error: ${e.message}',
+    };
+  }
+
+  /// Firebase Firestore Exception 분기 및 다이얼로그 컨텐츠에 들어갈 메세지 반환
+  String _handleFirestoreException(FirebaseException e) {
+    return switch (e.code) {
+      'aborted' => '요청이 중단되었습니다.',
+      'already-exists' => '이미 존재하는 데이터입니다.',
+      'cancelled' => '요청이 취소되었습니다.',
+      'data-loss' => '데이터가 손실되었습니다.',
+      'deadline-exceeded' => '기한이 초과된 요청입니다.',
+      'failed-precondition' => 'Firestore 오류: 사전 조건 실패',
+      'internal' => '서버가 불안정합니다.',
+      'invalid-argument' => '잘못된 데이터 인자 입니다.',
+      'not-found' => '데이터를 찾을수 없습니다.',
+      'out-of-range' => '범위 크기를 초과하였습니다.',
+      'permission-denied' => '요청할수 있는 권한이 없습니다.',
+      'resource-exhausted' => '더이상 데이터가 없습니다.',
+      'unauthenticated' => '인증되지 않는 요청입니다.',
+      'unavailable' => '이용이 불가능한 요청입니다.',
+      'unimplemented' => '구현되지 않는 요청입니다.',
+      'unknown' => '알 수 없는 에러입니다.',
+      _ => 'Firestore Error: ${e.message}',
+    };
+  }
+
+  /// Firebase Storage Exception 분기 및 다이얼로그 컨텐츠에 들어갈 메세지 반환
+  String _handleStorageException(FirebaseException e) {
+    return switch (e.code) {
+      'object-not-found' => '파일 객체를 찾을 수 없습니다.',
+      'bucket-not-found' => '버킷을 찾을 수 없습니다.',
+      'project-not-found' => '프로젝트를 찾을 수 없습니다.',
+      'quota-exceeded' => '할당량이 초과 되었습니다.',
+      'unauthenticated' => '인증되지 않은 요청입니다.',
+      'unauthorized' => '권한이 없는 요청입니다.',
+      'retry-limit-exceeded' => '재시도 한도 초과 되었습니다.',
+      'invalid-checksum' => '잘못된 체크섬',
+      'canceled' => '요청이 취소 되었습니다.',
+      'invalid-event-name' => '이벤트 이름이 잘못 되었습니다.',
+      'invalid-url' => '잘못된 URL 입니다.',
+      'invalid-argument' => '잘못된 인수 입니다.',
+      'no-default-bucket' => '기본 버킷이 없습니다.',
+      'cannot-slice-blob' => 'Blob을 자를 수 없습니다.',
+      'server-file-wrong-size' => '서버 파일 크기 오류 입니다.',
+      _ => 'Storage Error: ${e.message}',
+    };
+  }
+
   /// 비즈니스 로직에서 발생하는 예외 분기 및 다이얼로그 컨텐츠에 들어갈 메세지 반환
   String _businessExceptionHandle(CustomBusinessException e) {
     log(e.message);
@@ -50,69 +114,6 @@ class ExceptionMessageHandler {
       InternalServerException _ => e.message,
       NotFoundException _ => e.message,
       _ => '알수없는 에러가 발생하였습니다.${e.message}',
-    };
-  }
-
-  /// Firebase Auth Exception 분기 및 다이얼로그 컨텐츠에 들어갈 메세지 반환
-  String _handleAuthException(FirebaseException e) {
-    log(e.message ?? 'Auth Error');
-
-    return switch (e.code) {
-      'user-not-found' => 'Auth Error: User not found',
-      'wrong-password' => 'Auth Error: Wrong password',
-      'email-already-in-use' => 'Auth Error: Email already in use',
-      'invalid-email' => 'Auth Error: Invalid email',
-      'operation-not-allowed' => 'Auth Error: Operation not allowed',
-      'weak-password' => 'Auth Error: Weak password',
-      'network-request-failed' => 'Auth Error: Network request failed',
-      'too-many-requests' => 'Auth Error: Too many requests',
-      'user-disabled' => 'Auth Error: User account disabled',
-      _ => 'Auth Error: ${e.message}',
-    };
-  }
-
-  /// Firebase Firestore Exception 분기 및 다이얼로그 컨텐츠에 들어갈 메세지 반환
-  String _handleFirestoreException(FirebaseException e) {
-    return switch (e.code) {
-      'aborted' => 'Firestore Error: Aborted',
-      'already-exists' => 'Firestore Error: Already exists',
-      'cancelled' => 'Firestore Error: Cancelled',
-      'data-loss' => 'Firestore Error: Data loss',
-      'deadline-exceeded' => 'Firestore Error: Deadline exceeded',
-      'failed-precondition' => 'Firestore Error: Failed precondition',
-      'internal' => 'Firestore Error: Internal',
-      'invalid-argument' => 'Firestore Error: Invalid argument',
-      'not-found' => 'Firestore Error: Not found',
-      'out-of-range' => 'Firestore Error: Out of range',
-      'permission-denied' => 'Firestore Error: Permission denied',
-      'resource-exhausted' => 'Firestore Error: Resource exhausted',
-      'unauthenticated' => 'Firestore Error: Unauthenticated',
-      'unavailable' => 'Firestore Error: Unavailable',
-      'unimplemented' => 'Firestore Error: Unimplemented',
-      'unknown' => 'Firestore Error: Unknown',
-      _ => 'Firestore Error: ${e.message}',
-    };
-  }
-
-  /// Firebase Storage Exception 분기 및 다이얼로그 컨텐츠에 들어갈 메세지 반환
-  String _handleStorageException(FirebaseException e) {
-    return switch (e.code) {
-      'object-not-found' => 'Storage Error: Object not found',
-      'bucket-not-found' => 'Storage Error: Bucket not found',
-      'project-not-found' => 'Storage Error: Project not found',
-      'quota-exceeded' => 'Storage Error: Quota exceeded',
-      'unauthenticated' => 'Storage Error: Unauthenticated',
-      'unauthorized' => 'Storage Error: Unauthorized',
-      'retry-limit-exceeded' => 'Storage Error: Retry limit exceeded',
-      'invalid-checksum' => 'Storage Error: Invalid checksum',
-      'canceled' => 'Storage Error: Canceled',
-      'invalid-event-name' => 'Storage Error: Invalid event name',
-      'invalid-url' => 'Storage Error: Invalid URL',
-      'invalid-argument' => 'Storage Error: Invalid argument',
-      'no-default-bucket' => 'Storage Error: No default bucket',
-      'cannot-slice-blob' => 'Storage Error: Cannot slice blob',
-      'server-file-wrong-size' => 'Storage Error: Server file wrong size',
-      _ => 'Storage Error: ${e.message}',
     };
   }
 }
