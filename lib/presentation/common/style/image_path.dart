540ae6aea43a1611d6c997bda26fb9b6a011f875
YunZi.Song <yoonji5809@gmail.com>
2024-05-18 23:14:53 +0900

    Modify: colors, imagePath 새 경로에 파일 추가

---
+++ lib/presentation/common/style/image_path.dart
@@ -0,0 +1,57 @@
+class ImagePath {
+  static const String _iconPath = 'asset/icon';
+  static const String _imagePath = 'asset/image';
+
+  static const String temperatureDownArrowIcon =
+      '$_iconPath/temperature_down_arrow_icon.png';
+
+  static const String temperatureUpArrowIcon =
+      '$_iconPath/temperature_up_arrow_icon.png';
+
+  static const String weatherClearSkyIcon =
+      '$_iconPath/weather_clear_sky_icon.png';
+
+  static const String weatherPartlyCloudy =
+      '$_iconPath/weather_partly_cloudy_icon.png';
+
+  static const String weatherFog = '$_iconPath/weather_fog_icon.png';
+
+  static const String weatherDrizzle = '$_iconPath/weather_drizzle_icon.png';
+
+  static const String weatherRain = '$_iconPath/weather_rain_icon.png';
+
+  static const String weatherRainShower =
+      '$_iconPath/weather_rain_shower_icon.png';
+
+  static const String weatherFreezingRain =
+      '$_iconPath/weather_freezing_rain_icon.png';
+
+  static const String weatherSnow = '$_iconPath/weather_snow_icon.png';
+
+  static const String weatherThunderStorm =
+      '$_iconPath/weather_thunder_storm_icon.png';
+
+  static const String weatherThunderStormHail =
+      '$_iconPath/weather_thunder_storm_hail_icon.png';
+
+  static const String homeBackgroundSunny = '$_imagePath/home_bg_sunny.jpg';
+
+  static const String imageIconHome = '$_iconPath/weaco_home_weather_icon.png';
+
+  static const String imageIconFeed = '$_iconPath/weaco_feed_icon.png';
+
+  static const String imageIconSearch = '$_iconPath/weaco_search_icon.png';
+
+  static const String imageIconMyPage = '$_iconPath/weaco_mypage_icon.png';
+
+  static const String imageIconFeedAdd = '$_iconPath/weaco_feed_add_icon.png';
+
+  static const String imageIconCam = '$_iconPath/weaco_cam_icon.png';
+
+  static const String imageIconPhoto = '$_iconPath/weaco_photo_icon.png';
+
+  static const String weacoLogo =
+      'https://firebasestorage.googleapis.com/v0/b/weaco-f9878.appspot.com/o/3.001.png?alt=media&token=da2ba52d-25b0-4095-8a31-246830c0d1fa';
+  static const String weacoLogoAppBar = '$_imagePath/logo_app_bar.png';
+  static const String weacoLogoSplash = '$_imagePath/logo_splash.png';
+}
