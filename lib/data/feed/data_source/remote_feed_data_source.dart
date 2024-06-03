d865670ba66e435f048b5ddba8eccf1f9664ca0c
KimDonghyeok <qoophon@gmail.com>
2024-06-03 17:52:52 +0900

    Feat: 트랜잭션 인자 추가

--- lib/data/feed/data_source/remote_feed_data_source.dart
+++ lib/data/feed/data_source/remote_feed_data_source.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:weaco/domain/feed/model/feed.dart';
 import 'package:weaco/domain/weather/model/daily_location_weather.dart';
 
@@ -5,7 +6,10 @@ abstract interface class RemoteFeedDataSource {
   /// OOTD 피드 작성 성공 시 : 피드 업로드 요청(Feed) -> / 업로드 완료(bool) ← 파베
   /// OOTD 편집 완료 후 [상세 페이지]:  위와 동일.
   /// OOTD 편집 완료 후 [마이 페이지]: 위와 동일.*피드 업데이트
-  Future<bool> saveFeed({required Feed feed});
+  Future<bool> saveFeed({
+    required Transaction transaction,
+    required Feed feed,
+  });
 
   /// OOTD 피드 [상세 페이지] : 피드 데이터 요청 (id) -> 파베 / 피드 데이터 반환(json) ← 파베
   Future<Feed> getFeed({required String id});
@@ -18,17 +22,19 @@ abstract interface class RemoteFeedDataSource {
   });
 
   /// [마이페이지] 피드 삭제: 피드 삭제 요청(id) -> 파베/ 삭제 완료 (bool) from FB
-  Future<bool> deleteFeed({required String id});
+  Future<bool> deleteFeed({
+    required Transaction transaction,
+    required String id,
+  });
 
   /// [홈 화면] 하단 OOTD 추천 목록:
   ///
   /// 유저의 위치와 기온을 기반으로 피드 목록을 불러옵니다.
   /// @param city: 유저 위치의 도시명
   /// @param temperature: 날씨 온도
-  Future<List<Feed>> getRecommendedFeedList({
-    required DailyLocationWeather dailyLocationWeather,
-    DateTime? createdAt
-  });
+  Future<List<Feed>> getRecommendedFeedList(
+      {required DailyLocationWeather dailyLocationWeather,
+      DateTime? createdAt});
 
   /// [검색 페이지] 피드 검색:
   ///
