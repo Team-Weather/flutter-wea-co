794679c827b1ee9df85bdcb8d66eeb1cf1d4e4af
nala0897@naver.com <nala0897@naver.com>
2024-06-06 17:47:23 +0900

    Feat: RemoteFeedDataSourceImpl 커스텀 예외 처리 적용

--- lib/data/feed/data_source/remote_feed_data_source_impl.dart
+++ lib/data/feed/data_source/remote_feed_data_source_impl.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dio/dio.dart';
 import 'package:weaco/core/enum/exception_code.dart';
+import 'package:weaco/core/exception/internal_server_exception.dart';
+import 'package:weaco/core/exception/network_exception.dart';
 import 'package:weaco/core/exception/not_found_exception.dart';
 import 'package:weaco/core/firebase/firestore_dto_mapper.dart';
 import 'package:weaco/data/feed/data_source/remote_feed_data_source.dart';
@@ -15,39 +18,44 @@ class RemoteFeedDataSourceImpl implements RemoteFeedDataSource {
 
   /// OOTD 피드 작성 또는 편집 후 저장
   @override
-  Future<bool> saveFeed({required Feed feed}) async {
+  Future<void> saveFeed({required Feed feed}) async {
     final feedDto = toFeedDto(feed: feed);
 
-    // 피드를 수정 할 경우
-    if (feed.id != null) {
-      return await _fireStore
-          .collection('feeds')
-          .doc(feed.id)
-          .set(feedDto)
-          .then((value) => true);
-    }
+    try {
+      // 피드를 수정 할 경우
+      if (feed.id != null) {
+        await _fireStore
+            .collection('feeds')
+            .doc(feed.id)
+            .set(feedDto)
+            .then((value) => true);
+      }
 
-    // 새 피드를 저장 할 경우
-    return await _fireStore
-        .collection('feeds')
-        .add(feedDto)
-        .then((value) => true);
+      // 새 피드를 저장 할 경우
+      await _fireStore.collection('feeds').add(feedDto).then((value) => true);
+    } catch (e) {
+      _exceptionHandling(e);
+    }
   }
 
   /// [OOTD 피드 상세 페이지]:
   /// 피드 데이터 요청 (id) -> 파베 / 피드 데이터 반환(json) ← 파베
   @override
   Future<Feed> getFeed({required String id}) async {
-    final docSnapshot = await _fireStore.collection('feeds').doc(id).get();
+    try {
+      final docSnapshot = await _fireStore.collection('feeds').doc(id).get();
 
-    if (docSnapshot.data() == null) {
-      throw NotFoundException(
-        code: ExceptionCode.notFoundException,
-        message: '피드가 존재하지 않습니다.',
-      );
+      if (docSnapshot.data() == null) {
+        throw NotFoundException(
+          code: ExceptionCode.notFoundException,
+          message: '피드가 존재하지 않습니다.',
+        );
+      }
+
+      return toFeed(json: docSnapshot.data()!, id: docSnapshot.id);
+    } catch (e) {
+      throw _exceptionHandling(e);
     }
-
-    return toFeed(json: docSnapshot.data()!, id: docSnapshot.id);
   }
 
   /// [유저 페이지/마이 페이지]:
@@ -58,30 +66,44 @@ class RemoteFeedDataSourceImpl implements RemoteFeedDataSource {
     DateTime? createdAt,
     required int limit,
   }) async {
-    final querySnapshot = await _fireStore
-        .collection('feeds')
-        .where('user_email', isEqualTo: email)
-        .where('deleted_at', isNull: true)
-        .where('created_at',
-            isLessThan: createdAt ?? Timestamp.fromDate(DateTime.now()))
-        .orderBy('created_at', descending: true)
-        .limit(limit)
-        .get();
+    try {
+      final querySnapshot = await _fireStore
+          .collection('feeds')
+          .where('user_email', isEqualTo: email)
+          .where('deleted_at', isNull: true)
+          .where('created_at',
+              isLessThan: createdAt ?? Timestamp.fromDate(DateTime.now()))
+          .orderBy('created_at', descending: true)
+          .limit(limit)
+          .get();
 
-    return querySnapshot.docs
-        .map((doc) => toFeed(json: doc.data(), id: doc.id))
-        .toList();
+      if (querySnapshot.docs.isEmpty) {
+        throw NotFoundException(
+          code: ExceptionCode.notFoundException,
+          message: '피드가 존재하지 않습니다.',
+        );
+      }
+
+      return querySnapshot.docs
+          .map((doc) => toFeed(json: doc.data(), id: doc.id))
+          .toList();
+    } catch (e) {
+      throw _exceptionHandling(e);
+    }
   }
 
   /// [마이페이지] 피드 삭제
   /// soft delete 처리
   @override
-  Future<bool> deleteFeed({required String id}) async {
-    await _fireStore
-        .collection('feeds')
-        .doc(id)
-        .update({'deleted_at': Timestamp.fromDate(DateTime.now())});
-    return true;
+  Future<void> deleteFeed({required String id}) async {
+    try {
+      await _fireStore
+          .collection('feeds')
+          .doc(id)
+          .update({'deleted_at': Timestamp.fromDate(DateTime.now())});
+    } catch (e) {
+      _exceptionHandling(e);
+    }
   }
 
   /// [홈 페이지] 하단 OOTD 추천:
@@ -93,25 +115,30 @@ class RemoteFeedDataSourceImpl implements RemoteFeedDataSource {
       DateTime? createdAt}) async {
     final index = DateTime.now().hour;
     final weather = dailyLocationWeather.weatherList[index];
-    final querySnapshot = await _fireStore
-        .collection('feeds')
-        .where('weather.code', isEqualTo: weather.code)
-        // .where('season_code', isEqualTo: dailyLocationWeather.seasonCode)
-        .where(
-          'weather.temperature',
-          isLessThanOrEqualTo: dailyLocationWeather.highTemperature,
-          isGreaterThanOrEqualTo: dailyLocationWeather.lowTemperature,
-        )
-        .where('created_at',
-            isLessThan: createdAt ?? Timestamp.fromDate(DateTime.now()))
-        .where('deleted_at', isNull: true)
-        .orderBy('created_at', descending: true)
-        .limit(10)
-        .get();
 
-    return querySnapshot.docs
-        .map((doc) => toFeed(json: doc.data(), id: doc.id))
-        .toList();
+    try {
+      final querySnapshot = await _fireStore
+          .collection('feeds')
+          .where('weather.code', isEqualTo: weather.code)
+          // .where('season_code', isEqualTo: dailyLocationWeather.seasonCode)
+          .where(
+            'weather.temperature',
+            isLessThanOrEqualTo: dailyLocationWeather.highTemperature,
+            isGreaterThanOrEqualTo: dailyLocationWeather.lowTemperature,
+          )
+          .where('created_at',
+              isLessThan: createdAt ?? Timestamp.fromDate(DateTime.now()))
+          .where('deleted_at', isNull: true)
+          .orderBy('created_at', descending: true)
+          .limit(10)
+          .get();
+
+      return querySnapshot.docs
+          .map((doc) => toFeed(json: doc.data(), id: doc.id))
+          .toList();
+    } catch (e) {
+      throw _exceptionHandling(e);
+    }
   }
 
   /// [검색 페이지] 피드 검색:
@@ -127,38 +154,57 @@ class RemoteFeedDataSourceImpl implements RemoteFeedDataSource {
     int? minTemperature,
     int? maxTemperature,
   }) async {
-    Query<Map<String, dynamic>> query = _fireStore.collection('feeds');
-    // 날씨 코드 필터링
-    if (weatherCode != null) {
-      query = query.where('weather.code', isEqualTo: weatherCode);
+    try {
+      Query<Map<String, dynamic>> query = _fireStore.collection('feeds');
+      // 날씨 코드 필터링
+      if (weatherCode != null) {
+        query = query.where('weather.code', isEqualTo: weatherCode);
+      }
+
+      // 온도 범위 필터링
+      if (minTemperature != null && maxTemperature != null) {
+        query = query
+            .where('weather.temperature', isLessThanOrEqualTo: maxTemperature)
+            .where('weather.temperature',
+                isGreaterThanOrEqualTo: minTemperature);
+      }
+
+      // 계절 코드 필터링
+      if (seasonCode != null) {
+        query = query.where('season_code', isEqualTo: seasonCode);
+      }
+
+      // 생성일 기준으로 정렬하여 제한된 수의 문서 가져오기
+      final QuerySnapshot<Map<String, dynamic>> querySnapshot = await query
+          .where('created_at',
+              isLessThan: createdAt ?? Timestamp.fromDate(DateTime.now()))
+          .where('deleted_at', isNull: true)
+          .orderBy(
+            'created_at',
+            descending: true,
+          )
+          .limit(limit)
+          .get();
+
+      return querySnapshot.docs
+          .map((doc) => toFeed(json: doc.data(), id: doc.id))
+          .toList();
+    } catch (e) {
+      throw _exceptionHandling(e);
     }
+  }
 
-    // 온도 범위 필터링
-    if (minTemperature != null && maxTemperature != null) {
-      query = query
-          .where('weather.temperature', isLessThanOrEqualTo: maxTemperature)
-          .where('weather.temperature', isGreaterThanOrEqualTo: minTemperature);
+  Exception _exceptionHandling(Object e) {
+    switch (e.runtimeType) {
+      case FirebaseException _:
+        return InternalServerException(
+            code: ExceptionCode.internalServerException, message: '서버 내부 오류');
+      case DioException _:
+        return NetworkException(
+            code: ExceptionCode.internalServerException,
+            message: '네트워크 오류 : $e');
+      default:
+        return e as Exception;
     }
-
-    // 계절 코드 필터링
-    if (seasonCode != null) {
-      query = query.where('season_code', isEqualTo: seasonCode);
-    }
-
-    // 생성일 기준으로 정렬하여 제한된 수의 문서 가져오기
-    final QuerySnapshot<Map<String, dynamic>> querySnapshot = await query
-        .where('created_at',
-            isLessThan: createdAt ?? Timestamp.fromDate(DateTime.now()))
-        .where('deleted_at', isNull: true)
-        .orderBy(
-          'created_at',
-          descending: true,
-        )
-        .limit(limit)
-        .get();
-
-    return querySnapshot.docs
-        .map((doc) => toFeed(json: doc.data(), id: doc.id))
-        .toList();
   }
 }
