1a23abf7f1e195638295550c8cdd366cd11f7bbf
KimDonghyeok <qoophon@gmail.com>
2024-06-07 13:25:37 +0900

    Feat: RecommendOotdWidget 필드 수정, SkeletonRecommendOotdWidget 위젯 작성

--- lib/presentation/home/component/recommend_ootd_widget.dart
+++ lib/presentation/home/component/recommend_ootd_widget.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:weaco/domain/feed/model/feed.dart';
 import 'package:weaco/presentation/common/component/cached_image_widget.dart';
 
 class RecommendOotdWidget extends StatelessWidget {
   const RecommendOotdWidget({
     super.key,
-    required this.feedList,
-    required this.index,
+    required this.feedImagePath,
   });
 
-  final List<Feed> feedList;
-  final int index;
+  final String feedImagePath;
 
   @override
   Widget build(BuildContext context) {
@@ -29,9 +26,33 @@ class RecommendOotdWidget extends StatelessWidget {
       child: ClipRRect(
         borderRadius: BorderRadius.circular(10),
         child: CachedImageWidget(
-          feedList[index].thumbnailImagePath,
+          feedImagePath,
         ),
-       
+      ),
+    );
+  }
+}
+
+class SkeletonRecommendOotdWidget extends StatelessWidget {
+  const SkeletonRecommendOotdWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 110,
+      height: 110 * 16 / 9,
+      margin: const EdgeInsets.only(right: 25),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: const [
+            BoxShadow(
+              color: Color.fromARGB(48, 95, 95, 95),
+              blurRadius: 2,
+              offset: Offset(5, 0),
+            )
+          ]),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
       ),
     );
   }
