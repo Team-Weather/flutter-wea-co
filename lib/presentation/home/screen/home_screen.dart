ef0b6d29d5bc623beba2015a42a19e6618ef04b9
KimDonghyeok <qoophon@gmail.com>
2024-06-07 14:17:44 +0900

    Feat: 화면 전체 로딩 제거, 홈화면 각 요소 컴포넌트 위젯으로 분리

--- lib/presentation/home/screen/home_screen.dart
+++ lib/presentation/home/screen/home_screen.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:weaco/core/util/reaction_util.dart';
 import 'package:weaco/domain/feed/model/feed.dart';
+import 'package:weaco/presentation/common/enum/exception_alert.dart';
 import 'package:weaco/presentation/common/style/image_path.dart';
 import 'package:weaco/core/enum/weather_code.dart';
-import 'package:weaco/presentation/common/enum/exception_alert.dart';
 import 'package:weaco/presentation/common/util/alert_util.dart';
 import 'package:weaco/presentation/home/component/recommend_ootd_list_widget.dart';
 import 'package:weaco/presentation/home/component/weather_by_time_list_widget.dart';
@@ -27,11 +27,12 @@ class _HomeScreenState extends State<HomeScreen> {
 
     Future.microtask(
       () async {
-        await context.read<HomeScreenViewModel>().initHomeScreen();
-        if (mounted) {
-          final tmp = context.read<HomeScreenViewModel>().precacheList;
-          for (Feed e in tmp) {
-            await precacheImage(CachedNetworkImageProvider(e.thumbnailImagePath), context);
+        context.read<HomeScreenViewModel>().initHomeScreen();
+        final tmp = context.read<HomeScreenViewModel>().precacheList;
+        for (Feed e in tmp) {
+          if (mounted) {
+            await precacheImage(
+                CachedNetworkImageProvider(e.thumbnailImagePath), context);
           }
         }
       },
@@ -67,14 +68,13 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: switch (viewModel.status) {
         HomeScreenStatus.error => const Center(child: Text('데이터를 불러올 수 없습니다.')),
-        HomeScreenStatus.loading =>
-          const Center(child: CircularProgressIndicator()),
         HomeScreenStatus.idle => const SizedBox(),
-        HomeScreenStatus.success => Container(
+        HomeScreenStatus.loading || HomeScreenStatus.success => Container(
             decoration: BoxDecoration(
               image: DecorationImage(
                 fit: BoxFit.fill,
-                image: NetworkImage(viewModel.backgroundImagePath),
+                image:
+                    CachedNetworkImageProvider(viewModel.backgroundImagePath),
               ),
             ),
             child: SafeArea(
@@ -95,37 +95,35 @@ class _HomeScreenState extends State<HomeScreen> {
                             marginHeight: 40,
                           )),
                           // city
-                          Text(
-                            viewModel.dailyLocationWeather!.location.city,
-                            style: const TextStyle(
-                              fontSize: 15,
-                              color: Colors.white,
-                            ),
-                          ),
+                          viewModel.dailyLocationWeather != null
+                              ? HomeScreenCityTextWidget(
+                                  city: viewModel
+                                      .dailyLocationWeather!.location.city)
+                              : const HomeScreenCityTextWidget(city: '-'),
                           const SizedBox(height: 4),
                           // weather code description
-                          Text(
-                            WeatherCode.fromValue(
-                                    viewModel.currentWeather!.code)
-                                .description,
-                            style: const TextStyle(
-                              fontSize: 15,
-                              color: Colors.white,
-                            ),
-                          ),
+                          viewModel.currentWeather != null
+                              ? HomeScreenCurrentWeatherTextWidget(
+                                  currentWeather: WeatherCode.fromValue(
+                                          viewModel.currentWeather!.code)
+                                      .description)
+                              : const HomeScreenCurrentWeatherTextWidget(
+                                  currentWeather: '-',
+                                ),
                           SizedBox(
                               height: ReactionUtil.reactHeight(
                             context: context,
                             marginHeight: 20,
                           )),
                           // current temperature
-                          Text(
-                            '${viewModel.currentWeather!.temperature}℃',
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 60,
-                            ),
-                          ),
+                          viewModel.currentWeather != null
+                              ? HomeScreenCurrentTemperatureTextWidget(
+                                  currentTemperature:
+                                      '${viewModel.currentWeather!.temperature}',
+                                )
+                              : const HomeScreenCurrentTemperatureTextWidget(
+                                  currentTemperature: '-',
+                                ),
                           SizedBox(
                               height: ReactionUtil.reactHeight(
                             context: context,
@@ -135,34 +133,25 @@ class _HomeScreenState extends State<HomeScreen> {
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: [
                               // max, min temperature
+
                               Column(
                                 children: [
-                                  Text(
-                                    '최고 ${viewModel.dailyLocationWeather!.highTemperature}℃',
-                                    style: const TextStyle(
-                                      fontSize: 15,
-                                      color: Colors.white,
-                                      shadows: [
-                                        Shadow(
-                                            blurRadius: 4,
-                                            color: Color.fromARGB(165, 0, 0, 0),
-                                            offset: Offset(1, 1)),
-                                      ],
-                                    ),
-                                  ),
-                                  Text(
-                                    '최저 ${viewModel.dailyLocationWeather!.lowTemperature}℃',
-                                    style: const TextStyle(
-                                      fontSize: 15,
-                                      color: Colors.white,
-                                      shadows: [
-                                        Shadow(
-                                            blurRadius: 4,
-                                            color: Color.fromARGB(165, 0, 0, 0),
-                                            offset: Offset(1, 1)),
-                                      ],
-                                    ),
-                                  ),
+                                  viewModel.dailyLocationWeather != null
+                                      ? HomeScreenDailyHighestTemperatureTextWidget(
+                                          highestTemperature:
+                                              '${viewModel.dailyLocationWeather!.highTemperature}',
+                                        )
+                                      : const HomeScreenDailyHighestTemperatureTextWidget(
+                                          highestTemperature: '-',
+                                        ),
+                                  viewModel.dailyLocationWeather != null
+                                      ? HomeScreenDailyLowestTemperatureTextWidget(
+                                          lowestTemperature:
+                                              '${viewModel.dailyLocationWeather!.lowTemperature}',
+                                        )
+                                      : const HomeScreenDailyLowestTemperatureTextWidget(
+                                          lowestTemperature: '-',
+                                        ),
                                 ],
                               ),
                               // 전일 대비
@@ -237,6 +226,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
                     // ootd list
                     RecommendOotdListWidget(
+                      isLoading: viewModel.isRecommendOotdLoading,
                       dailyLocationWeather: viewModel.dailyLocationWeather,
                       feedList: viewModel.feedList,
                     ),
@@ -246,6 +236,118 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
       },
+    );
+  }
+}
+
+class HomeScreenCityTextWidget extends StatelessWidget {
+  final String city;
+
+  const HomeScreenCityTextWidget({
+    super.key,
+    required this.city,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      city,
+      style: const TextStyle(
+        fontSize: 15,
+        color: Colors.white,
+      ),
+    );
+  }
+}
+
+class HomeScreenCurrentWeatherTextWidget extends StatelessWidget {
+  const HomeScreenCurrentWeatherTextWidget({
+    super.key,
+    required this.currentWeather,
+  });
+
+  final String currentWeather;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      currentWeather,
+      style: const TextStyle(
+        fontSize: 15,
+        color: Colors.white,
+      ),
+    );
+  }
+}
+
+class HomeScreenCurrentTemperatureTextWidget extends StatelessWidget {
+  const HomeScreenCurrentTemperatureTextWidget({
+    super.key,
+    required this.currentTemperature,
+  });
+
+  final String currentTemperature;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '$currentTemperature℃',
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 60,
+      ),
+    );
+  }
+}
+
+class HomeScreenDailyHighestTemperatureTextWidget extends StatelessWidget {
+  const HomeScreenDailyHighestTemperatureTextWidget({
+    super.key,
+    required this.highestTemperature,
+  });
+
+  final String highestTemperature;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '최고 $highestTemperature℃',
+      style: const TextStyle(
+        fontSize: 15,
+        color: Colors.white,
+        shadows: [
+          Shadow(
+              blurRadius: 4,
+              color: Color.fromARGB(165, 0, 0, 0),
+              offset: Offset(1, 1)),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeScreenDailyLowestTemperatureTextWidget extends StatelessWidget {
+  const HomeScreenDailyLowestTemperatureTextWidget({
+    super.key,
+    required this.lowestTemperature,
+  });
+
+  final String lowestTemperature;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '최저 $lowestTemperature℃',
+      style: const TextStyle(
+        fontSize: 15,
+        color: Colors.white,
+        shadows: [
+          Shadow(
+              blurRadius: 4,
+              color: Color.fromARGB(165, 0, 0, 0),
+              offset: Offset(1, 1)),
+        ],
+      ),
     );
   }
 }
