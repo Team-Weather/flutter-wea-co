7a03eb12bb11e69926b63d1ffa174647c859c956
KimDonghyeok <qoophon@gmail.com>
2024-06-08 19:51:29 +0900

    Feat: SkeletonRecommendOotdWidget 삭제, RecommendOotdWidget 의 feedImagePath 필드를 nullable 하게 하여 스켈레톤 구현

--- lib/presentation/home/component/recommend_ootd_widget.dart
+++ lib/presentation/home/component/recommend_ootd_widget.dart
@@ -4,10 +4,10 @@ import 'package:weaco/presentation/common/component/cached_image_widget.dart';
 class RecommendOotdWidget extends StatelessWidget {
   const RecommendOotdWidget({
     super.key,
-    required this.feedImagePath,
+    this.feedImagePath,
   });
 
-  final String feedImagePath;
+  final String? feedImagePath;
 
   @override
   Widget build(BuildContext context) {
@@ -25,34 +25,11 @@ class RecommendOotdWidget extends StatelessWidget {
           ]),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(10),
-        child: CachedImageWidget(
-          feedImagePath,
-        ),
-      ),
-    );
-  }
-}
-
-class SkeletonRecommendOotdWidget extends StatelessWidget {
-  const SkeletonRecommendOotdWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 110,
-      height: 110 * 16 / 9,
-      margin: const EdgeInsets.only(right: 25),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: const [
-            BoxShadow(
-              color: Color.fromARGB(48, 95, 95, 95),
-              blurRadius: 2,
-              offset: Offset(5, 0),
-            )
-          ]),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(10),
+        child: feedImagePath != null
+            ? CachedImageWidget(
+                feedImagePath!,
+              )
+            : null,
       ),
     );
   }
